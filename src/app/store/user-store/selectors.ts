01234e4de99126e20b3e8b4a07f8e590a082bb50
Martin Rodalgaard <mro@trifork.com>
2023-01-09 13:32:49 +0100

    Component state + mock load of frontpage content

--- src/app/store/user-store/selectors.ts
+++ src/app/store/user-store/selectors.ts
@@ -1,6 +1,3 @@
-import { createSelector } from '@ngrx/store';
 import { userFeature } from './reducer';
 
 export const { selectUser, selectUserIsFetching } = userFeature;
-
-export const selectUserName = createSelector(userFeature.selectUser, (user) => user?.name ?? '');
