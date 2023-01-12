22b571e0ba2522d858471e110780069ae21e84c3
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2023-01-12 14:37:17 +0100

    added error handling in build

--- DeploymentScripts/Publish.ps1
+++ DeploymentScripts/Publish.ps1
@@ -7,4 +7,7 @@ $ErrorActionPreference = 'Stop'
 Write-Host "PUBLISH: $environment"
 
 .$PSScriptRoot\Build.ps1 -environment $environment
+if ( -not $? ) { throw "Failed build" }
+
 Copy-Item "$PSScriptRoot\..\dist\kitos-web" -Destination "$deployment_packages_dir\$environment" -Recurse
+if ( -not $? ) { throw "Failed copy of dist" }
