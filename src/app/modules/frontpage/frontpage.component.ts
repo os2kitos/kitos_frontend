cae15dc3dd4fdf0578cb4dfa2b6d4c5c9325b4fe
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2023-01-18 08:21:04 +0100

    added trusted types enforcement

--- src/app/modules/frontpage/frontpage.component.ts
+++ src/app/modules/frontpage/frontpage.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, SecurityContext } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { Store } from '@ngrx/store';
+import { map } from 'rxjs/operators';
+import { APITextDTO } from 'src/app/api/v1';
 import { UserActions } from 'src/app/store/user-store/actions';
 import { selectUser } from 'src/app/store/user-store/selectors';
 import { FrontpageComponentStore } from './frontpage.component-store';
@@ -13,9 +16,17 @@ export class FrontpageComponent implements OnInit {
   public user$ = this.store.select(selectUser);
 
   public loading$ = this.frontpageComponentStore.loading$;
-  public text$ = this.frontpageComponentStore.text$;
+  public text$ = this.frontpageComponentStore
+    .text$
+    .pipe(map(texts =>
+      texts.map(text => {
+        return <APITextDTO>{
+          id: text.id,
+          text: this.sanitizer.sanitize(SecurityContext.HTML, text.value ?? "")
+        }
+      })));
 
-  constructor(private store: Store, private frontpageComponentStore: FrontpageComponentStore) {}
+  constructor(private store: Store, private frontpageComponentStore: FrontpageComponentStore, private readonly sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
     this.frontpageComponentStore.getText();
