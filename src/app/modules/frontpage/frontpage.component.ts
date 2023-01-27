2efc38eb48a971b2b1182dc809518a490ee42648
Martin Rodalgaard <mro@trifork.com>
2023-01-27 14:36:12 +0100

    Mark i18n text + Do not show login card after login

--- src/app/modules/frontpage/frontpage.component.ts
+++ src/app/modules/frontpage/frontpage.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { selectUser } from 'src/app/store/user-store/selectors';
 import { FrontpageComponentStore } from './frontpage.component-store';
 
 @Component({
@@ -10,7 +12,9 @@ export class FrontpageComponent implements OnInit {
   public loading$ = this.frontpageComponentStore.loading$;
   public text$ = this.frontpageComponentStore.text$;
 
-  constructor(private frontpageComponentStore: FrontpageComponentStore) {}
+  public user$ = this.store.select(selectUser);
+
+  constructor(private frontpageComponentStore: FrontpageComponentStore, private store: Store) {}
 
   ngOnInit(): void {
     this.frontpageComponentStore.getText();
