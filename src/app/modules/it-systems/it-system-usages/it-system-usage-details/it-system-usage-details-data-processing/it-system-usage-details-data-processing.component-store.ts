177906d8575e3022efda8c64e08898f422507239
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2023-03-14 14:05:58 +0100

    added basic list output from the component store

---
+++ src/app/modules/it-systems/it-system-usages/it-system-usage-details/it-system-usage-details-data-processing/it-system-usage-details-data-processing.component-store.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { ComponentStore, tapResponse } from '@ngrx/component-store';
+import { mergeMap, Observable } from 'rxjs';
+import { APIDataProcessingRegistrationResponseDTO, APIV2DataProcessingRegistrationService } from 'src/app/api/v2';
+import { filterNullish } from 'src/app/shared/pipes/filter-nullish';
+
+interface State {
+  dataProcessingRegistrations?: Array<APIDataProcessingRegistrationResponseDTO>
+}
+
+@Injectable({ providedIn: 'any' })
+export class ItSystemUsageDetailsDataProcessingComponentStore extends ComponentStore<State> {
+  public readonly associatedDataProcessingRegistrations$ = this
+    .select((state) => state.dataProcessingRegistrations)
+    .pipe(filterNullish());
+
+  constructor(private apiDataProcessingRegistrationService: APIV2DataProcessingRegistrationService) {
+    super({});
+  }
+
+  private updateAssociatedDataProcessingRegistrations = this.updater(
+    (state, dataProcessingRegistrations: Array<APIDataProcessingRegistrationResponseDTO>): State => ({
+      ...state,
+      dataProcessingRegistrations,
+    })
+  );
+
+  public getAssociatedDataProcessingRegistrations = this.effect((systemUsageUuid$: Observable<string>) =>
+    systemUsageUuid$.pipe(
+      mergeMap((systemUsageUuid) =>
+        this.apiDataProcessingRegistrationService.gETDataProcessingRegistrationV2GetDataProcessingRegistrationsBoundedPaginationQueryPaginationQueryNullable1AgreementConcludedNullable1ChangedSinceGtEqNullable1DataProcessorUuidNullable1OrganizationUuidNullable1SubDataProcessorUuidNullable1SystemUsageUuidNullable1SystemUuid(undefined, undefined, systemUsageUuid)
+          .pipe(
+            tapResponse(
+              (dataProcessingRegistrations) => this.updateAssociatedDataProcessingRegistrations(dataProcessingRegistrations),
+              (e) => console.error(e)
+            )
+          )
+      )
+    )
+  );
+}
--- src/app/modules/it-systems/it-system-usages/it-system-usage-details/it-system-usage-details-data-processing/it-system-usage-details-data-processing.component.ts
+++ src/app/modules/it-systems/it-system-usages/it-system-usage-details/it-system-usage-details-data-processing/it-system-usage-details-data-processing.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { BaseComponent } from 'src/app/shared/base/base.component';
+import { filterNullish } from 'src/app/shared/pipes/filter-nullish';
+import { selectItSystemUsage } from 'src/app/store/it-system-usage/selectors';
+import { ItSystemUsageDetailsDataProcessingComponentStore } from './it-system-usage-details-data-processing.component-store';
 
 @Component({
   selector: 'app-it-system-usage-details-data-processing',
   templateUrl: './it-system-usage-details-data-processing.component.html',
   styleUrls: ['./it-system-usage-details-data-processing.component.scss']
 })
-export class ItSystemUsageDetailsDataProcessingComponent {
-  constructor(private route: ActivatedRoute, private store: Store) { }
+export class ItSystemUsageDetailsDataProcessingComponent extends BaseComponent implements OnInit {
+  readonly dataProcessingRegistrations$ = this.dataProcessingComponentStore.associatedDataProcessingRegistrations$;
+
+  constructor(private store: Store, private dataProcessingComponentStore: ItSystemUsageDetailsDataProcessingComponentStore) {
+    super();
+  }
+
+  ngOnInit(): void {
+    this.subscriptions.add(
+      this.store
+        .select(selectItSystemUsage)
+        .pipe(filterNullish())
+        .subscribe((itSystemUsage) =>
+          this.dataProcessingComponentStore.getAssociatedDataProcessingRegistrations(itSystemUsage.uuid)
+        )
+    );
+  }
 }
