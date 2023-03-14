fbdd592fb836ae36f6c4cd590f4327bf48fe54d1
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2023-03-14 11:58:54 +0100

    added icon and dpr menu item

--- src/app/shared/components/icons/icons.module.ts
+++ src/app/shared/components/icons/icons.module.ts
@@ -4,6 +4,7 @@ import { ClipboardIconComponent } from './clipboard-icon.component';
 import { DocumentIconComponent } from './document-icon.component';
 import { ExportIconComponent } from './export-icon.component';
 import { FilterIconComponent } from './filter.component';
+import { FolderImportant } from './folder-important.component';
 import { HelpIconComponent } from './help.component';
 import { InfoIconComponent } from './info-icon.component';
 import { KitosKIconComponent } from './kitos-k-icon.component';
@@ -31,6 +32,7 @@ import { XIconComponent } from './x-icon.component';
     ClipboardIconComponent,
     HelpIconComponent,
     InfoIconComponent,
+    FolderImportant
   ],
   declarations: [
     ChevronDownIconComponent,
@@ -47,7 +49,8 @@ import { XIconComponent } from './x-icon.component';
     ClipboardIconComponent,
     HelpIconComponent,
     InfoIconComponent,
+    FolderImportant
   ],
   providers: [],
 })
-export class IconsModule {}
+export class IconsModule { }
