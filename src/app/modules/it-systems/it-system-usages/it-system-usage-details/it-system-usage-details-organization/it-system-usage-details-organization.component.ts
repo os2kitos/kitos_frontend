28c17d57cbb13186b8d24fa11945538d33283218
Aleksander Naskret <aln@strongminds.dk>
2023-03-21 12:01:05 +0100

    used by units table empty state

--- src/app/modules/it-systems/it-system-usages/it-system-usage-details/it-system-usage-details-organization/it-system-usage-details-organization.component.ts
+++ src/app/modules/it-systems/it-system-usages/it-system-usage-details/it-system-usage-details-organization/it-system-usage-details-organization.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { BaseComponent } from 'src/app/shared/base/base.component';
+import { filterNullish } from 'src/app/shared/pipes/filter-nullish';
+import { invertBooleanValue } from 'src/app/shared/pipes/invert-boolean-value';
+import { matchEmptyArray } from 'src/app/shared/pipes/match-empty-array';
+import {
+  selectItSystemUsageResponsibleUnit,
+  selectItSystemUsageUsingOrganizationUnits,
+} from 'src/app/store/it-system-usage/selectors';
+import { OrganizationUnitActions } from 'src/app/store/organization-unit/actions';
+import { selectOrganizationUnits } from 'src/app/store/organization-unit/selectors';
+import { selectOrganizationUuid } from 'src/app/store/user-store/selectors';
 
 @Component({
   selector: 'app-it-system-usage-details-organization',
   templateUrl: './it-system-usage-details-organization.component.html',
-  styleUrls: ['./it-system-usage-details-organization.component.scss']
+  styleUrls: ['./it-system-usage-details-organization.component.scss'],
 })
-export class ItSystemUsageDetailsOrganizationComponent {
+export class ItSystemUsageDetailsOrganizationComponent extends BaseComponent implements OnInit {
+  public readonly responsibleUnit$ = this.store.select(selectItSystemUsageResponsibleUnit).pipe(filterNullish());
+  public readonly usedByUnits$ = this.store.select(selectItSystemUsageUsingOrganizationUnits).pipe(filterNullish());
+  public readonly anyUsedByUnits$ = this.usedByUnits$.pipe(matchEmptyArray(), invertBooleanValue());
+  public readonly organizationUnits$ = this.store.select(selectOrganizationUnits);
 
+  constructor(private store: Store) {
+    super();
+  }
+
+  onAddNew() {
+    console.log('CLICK!');
+  }
+
+  ngOnInit(): void {
+    this.subscriptions.add(
+      this.store
+        .select(selectOrganizationUuid)
+        .pipe(filterNullish())
+        .subscribe((organizationUuid) =>
+          this.store.dispatch(OrganizationUnitActions.getOrganizationUnits(organizationUuid))
+        )
+    );
+  }
 }
