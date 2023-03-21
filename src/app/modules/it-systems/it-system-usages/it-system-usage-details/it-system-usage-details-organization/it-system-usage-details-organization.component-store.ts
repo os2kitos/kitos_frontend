bc6d65ff0ca34284f8e333af315512abf525dad1
Aleksander Naskret <aln@strongminds.dk>
2023-03-21 08:46:00 +0100

    cards

--- src/app/modules/it-systems/it-system-usages/it-system-usage-details/it-system-usage-details-organization/it-system-usage-details-organization.component-store.ts
+++ src/app/modules/it-systems/it-system-usages/it-system-usage-details/it-system-usage-details-organization/it-system-usage-details-organization.component-store.ts
@@ -1,34 +1,34 @@
-import { Injectable } from '@angular/core';
-import { ComponentStore } from '@ngrx/component-store';
-import { APIOrganizationUnitResponseDTO, APIV2OrganizationService } from 'src/app/api/v2';
-import { filterNullish } from 'src/app/shared/pipes/filter-nullish';
+// import { Injectable } from '@angular/core';
+// import { ComponentStore } from '@ngrx/component-store';
+// import { APIOrganizationUnitResponseDTO, APIV2OrganizationService } from 'src/app/api/v2';
+// import { filterNullish } from 'src/app/shared/pipes/filter-nullish';
 
-interface State {
-  loading: boolean;
-  organizationUnits?: Array<APIOrganizationUnitResponseDTO>;
-}
+// interface State {
+//   loading: boolean;
+//   organizationUnits?: Array<APIOrganizationUnitResponseDTO>;
+// }
 
-@Injectable()
-export class ItSystemOrganizationComponentStore extends ComponentStore<State> {
-  public readonly organizationUnits$ = this.select((state) => state.organizationUnits).pipe(filterNullish());
+// @Injectable()
+// export class ItSystemOrganizationComponentStore extends ComponentStore<State> {
+//   public readonly organizationUnits$ = this.select((state) => state.organizationUnits).pipe(filterNullish());
 
-  public readonly organizationUnitsIsLoading$ = this.select((state) => state.loading).pipe(filterNullish());
+//   public readonly organizationUnitsIsLoading$ = this.select((state) => state.loading).pipe(filterNullish());
 
-  constructor(private apiOrganziationUnitService: APIV2OrganizationService) {
-    super({ loading: false });
-  }
+//   constructor(private apiOrganziationUnitService: APIV2OrganizationService) {
+//     super({ loading: false });
+//   }
 
-  private updateOrganizationUnits = this.updater(
-    (state, organizationUnits: Array<APIOrganizationUnitResponseDTO>): State => ({
-      ...state,
-      organizationUnits,
-    })
-  );
+//   private updateOrganizationUnits = this.updater(
+//     (state, organizationUnits: Array<APIOrganizationUnitResponseDTO>): State => ({
+//       ...state,
+//       organizationUnits,
+//     })
+//   );
 
-  private updateOrganziationUnitsIsLoading = this.updater(
-    (state, loading: boolean): State => ({
-      ...state,
-      loading,
-    })
-  );
-}
+//   private updateOrganziationUnitsIsLoading = this.updater(
+//     (state, loading: boolean): State => ({
+//       ...state,
+//       loading,
+//     })
+//   );
+// }
