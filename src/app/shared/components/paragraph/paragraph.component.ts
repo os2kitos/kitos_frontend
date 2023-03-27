1f3fd6cb95cc50fa285d02772685b6523a3f2638
Aleksander Naskret <aln@strongminds.dk>
2023-03-27 10:15:25 +0200

    lint fix

--- src/app/shared/components/paragraph/paragraph.component.ts
+++ src/app/shared/components/paragraph/paragraph.component.ts
@@ -8,5 +8,5 @@ import { ParagraphFontSizeTypes } from '../../models/sizes/paragraph-font-sizes.
 })
 export class ParagraphComponent {
   @Input() public paragraphSize: ParagraphFontSizeTypes = 'small';
-  @Input() public withOverlow: boolean = false;
+  @Input() public withOverlow = false;
 }
