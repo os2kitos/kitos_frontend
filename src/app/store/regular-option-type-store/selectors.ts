9b2055828cec5b2131c5276e8c70cb3f8f7504a5
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2023-03-28 09:31:33 +0200

    memorizing selectors for regular options

--- src/app/store/regular-option-type-store/selectors.ts
+++ src/app/store/regular-option-type-store/selectors.ts
@@ -5,18 +5,43 @@ import { regularOptionTypeAdapter, regularOptionTypeFeature } from './reducer';
 
 const { selectRegularOptionTypeState } = regularOptionTypeFeature;
 
+const selectStateByOptionTypeCache: { [key: string]: unknown } = {};
 export const selectStateByOptionType = (optionType: RegularOptionTypes) =>
-  createSelector(selectRegularOptionTypeState, (state) => state[optionType]);
-
-export const selectRegularOptionTypes = (optionType: RegularOptionTypes) =>
-  createSelector(selectStateByOptionType(optionType), regularOptionTypeAdapter.getSelectors().selectAll);
-
-export const selectRegularOptionTypesDictionary = (optionType: RegularOptionTypes) =>
-  createSelector(selectStateByOptionType(optionType), regularOptionTypeAdapter.getSelectors().selectEntities);
-
-export const selectHasValidCache = (optionType: RegularOptionTypes) =>
-  createSelector(
-    selectRegularOptionTypeState,
-    () => new Date(),
-    (state, time) => hasValidCache(state[optionType].cacheTime, time)
+  memorizedSelector(optionType, selectStateByOptionTypeCache, (optionType) =>
+    createSelector(selectRegularOptionTypeState, (state) => state[optionType])
   );
+
+const selectRegularOptionTypesCache: { [key: string]: unknown } = {};
+export const selectRegularOptionTypes = (optionType: RegularOptionTypes) =>
+  memorizedSelector(optionType, selectRegularOptionTypesCache, (optionType) =>
+    createSelector(selectStateByOptionType(optionType), regularOptionTypeAdapter.getSelectors().selectAll)
+  );
+
+const selectRegularOptionTypesDictionaryCache: { [key: string]: unknown } = {};
+export const selectRegularOptionTypesDictionary = (optionType: RegularOptionTypes) =>
+  memorizedSelector(optionType, selectRegularOptionTypesDictionaryCache, (optionType) =>
+    createSelector(selectStateByOptionType(optionType), regularOptionTypeAdapter.getSelectors().selectEntities)
+  );
+
+const selectHasValidCacheCache: { [key: string]: unknown } = {};
+export const selectHasValidCache = (optionType: RegularOptionTypes) =>
+  memorizedSelector(optionType, selectHasValidCacheCache, (optionType) =>
+    createSelector(
+      selectRegularOptionTypeState,
+      () => new Date(),
+      (state, time) => hasValidCache(state[optionType].cacheTime, time)
+    )
+  );
+
+function memorizedSelector<TSelector>(
+  optionType: RegularOptionTypes,
+  cache: { [key: string]: unknown },
+  factory: (optionType: RegularOptionTypes) => TSelector
+): TSelector {
+  let memorizedSelector = cache[optionType];
+  if (!memorizedSelector) {
+    memorizedSelector = factory(optionType);
+    cache[optionType] = memorizedSelector;
+  }
+  return memorizedSelector as TSelector;
+}
