35d49e949c8168d9289d17a140d3ab7bfe166d0b
Aleksander Naskret <aln@strongminds.dk>
2023-04-17 13:57:30 +0200

    subtree sort fix

--- src/app/shared/components/tree-node-dropdown/tree-node-dropdown.component.ts
+++ src/app/shared/components/tree-node-dropdown/tree-node-dropdown.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Dictionary } from '@ngrx/entity';
 import { BaseDropdownComponent } from '../../base/base-dropdown.component';
+import { Dictionary } from '../../models/primitives/dictionary.model';
 import { TreeNodeModel } from '../../models/tree-node.model';
 
 @Component({
@@ -16,7 +16,7 @@ export class TreeNodeDropdownComponent extends BaseDropdownComponent<TreeNodeMod
   public sortedData: TreeNodeModel[] = [];
 
   public itemParentIdsDictionary: Dictionary<string[]> = {};
-  private lookup: { searchName: string; unitIdsWithMatchingName: string[]; foundUnitsParentIds: string[] } = {
+  private searchResult: { searchName: string; unitIdsWithMatchingName: string[]; foundUnitsParentIds: string[] } = {
     searchName: '',
     unitIdsWithMatchingName: [],
     foundUnitsParentIds: [],
@@ -26,34 +26,22 @@ export class TreeNodeDropdownComponent extends BaseDropdownComponent<TreeNodeMod
     super.ngOnInit();
 
     if (!this.data) return;
-    const dataDictionary = Object.fromEntries(this.data.map((x) => [x.id, x]));
-
-    //create a dictionary for items and their parentIds for a quick lookup
-    this.itemParentIdsDictionary = Object.fromEntries(
-      this.data.map((item) => {
-        const node = item as TreeNodeModel;
-        const result = this.getParents(node, dataDictionary).map((x) => x.id);
-        return [item.id, result];
-      })
+    const childrenDictionary = Object.fromEntries(
+      this.data.map((item) => [item.id, this.data?.filter((x) => x.parentId === item.id)])
     );
 
-    //sort the array so it appears in the correct order
-    this.data.forEach((item) => {
-      if (!item.parentId) {
-        this.sortedData.push(item);
-        return;
-      }
-      const parent = this.sortedData.find((x) => x.id === item.parentId);
-      if (parent) {
-        this.sortedData.splice(this.sortedData.indexOf(parent) + 1, 0, item);
-      }
-    });
+    //Build final selection/lookup model
+    this.data
+      .filter((x) => !x.parentId)
+      .forEach((root) => {
+        this.processSubtree(root, [], childrenDictionary);
+      });
   }
 
-  public searchWitItemParents = (searchName: string, item: TreeNodeModel) => {
+  public searchWithItemParents = (searchName: string, item: TreeNodeModel) => {
     const treeNodes = this.data as TreeNodeModel[];
 
-    if (this.lookup.searchName !== searchName) {
+    if (this.searchResult.searchName !== searchName) {
       //get ids of nodes that match the search name
       const nodeIds = treeNodes
         .filter((x) => x.name.toLocaleLowerCase().includes(searchName.toLocaleLowerCase()))
@@ -64,19 +52,26 @@ export class TreeNodeDropdownComponent extends BaseDropdownComponent<TreeNodeMod
       nodeIds.forEach((id) => {
         parents = parents.concat(this.itemParentIdsDictionary[id] ?? []);
       });
-      this.lookup = { searchName: searchName, unitIdsWithMatchingName: nodeIds, foundUnitsParentIds: parents };
+      this.searchResult = { searchName: searchName, unitIdsWithMatchingName: nodeIds, foundUnitsParentIds: parents };
     }
 
-    return this.lookup.unitIdsWithMatchingName.includes(item.id) || this.lookup.foundUnitsParentIds.includes(item.id);
+    return (
+      this.searchResult.unitIdsWithMatchingName.includes(item.id) ||
+      this.searchResult.foundUnitsParentIds.includes(item.id)
+    );
   };
 
-  private getParents(item: TreeNodeModel, data: Dictionary<TreeNodeModel>): TreeNodeModel[] {
-    let result = [item] as TreeNodeModel[];
+  private processSubtree(
+    currentItem: TreeNodeModel,
+    ancestors: Array<string>,
+    childrenDictionary: Dictionary<TreeNodeModel[]>
+  ) {
+    this.itemParentIdsDictionary[currentItem.id] = ancestors;
+    this.sortedData.push(currentItem);
 
-    const parent = data[item.parentId];
-    if (!parent) return result;
-    result = result.concat(this.getParents(parent, data));
+    const children = childrenDictionary[currentItem.id];
+    children?.sort((a, b) => a.name.localeCompare(b.name));
 
-    return result;
+    children?.forEach((child) => this.processSubtree(child, ancestors.concat(currentItem.id), childrenDictionary));
   }
 }
