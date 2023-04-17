525d67bd268087045cb99eb2b11a2edb5b562fc8
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2023-04-17 10:20:50 +0200

    rewritten "trimmer" to use an iterative approach in stead of recursion

--- src/app/shared/helpers/string.helpers.ts
+++ src/app/shared/helpers/string.helpers.ts
@@ -5,12 +5,19 @@
  * @returns
  */
 export function removeAllLeadingAndTrailingSlashes(input: string): string {
-  if (!input) {
-    return input;
-  } else if (input.endsWith('/')) {
-    return removeAllLeadingAndTrailingSlashes(input.slice(0, -1));
-  } else if (input.startsWith('/')) {
-    return removeAllLeadingAndTrailingSlashes(input.slice(1));
-  }
-  return input;
+  let trimmed = false;
+  let trimmedInput = input;
+  do {
+    if (!trimmedInput) {
+      trimmed = true;
+    } else if (trimmedInput.endsWith('/')) {
+      trimmedInput = trimmedInput.slice(0, -1);
+    } else if (trimmedInput.startsWith('/')) {
+      trimmedInput = trimmedInput.slice(1);
+    } else {
+      trimmed = true;
+    }
+  } while (!trimmed);
+
+  return trimmedInput;
 }
