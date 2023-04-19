14859b0ee5ba0ed32a0809373dc0d112cca05e14
Aleksander Naskret <aln@strongminds.dk>
2023-04-19 15:37:10 +0200

    e2e tests

--- cypress/e2e/it-system-usage.cy.ts
+++ cypress/e2e/it-system-usage.cy.ts
@@ -451,4 +451,91 @@ describe('it-system-usage', () => {
     //validate selected unit was updated
     cy.contains('Ansvarlig organisationsenhed').parentsUntil('app-card').parent().should('contain', 'Test - 1');
   });
+
+  it('can show System roles', () => {
+    cy.intercept('/api/v2/it-system-usages/*', { fixture: 'it-system-usage.json' });
+
+    cy.contains('System 3').click();
+
+    cy.intercept('/api/v2/it-system-usage-role-types*', { fixture: 'it-system-usage-available-roles.json' });
+    cy.intercept('/api/v2/**/roles', { fixture: 'it-system-usage-roles.json' });
+
+    cy.navigateToDetailsSubPage('Systemroller');
+
+    const expectedRows = [
+      {
+        user: {
+          email: 'local-global-admin-user@kitos.dk',
+          name: 'Automatisk oprettet testbruger (GlobalAdmin)',
+        },
+        role: { name: 'Changemanager' },
+        writeAccess: true,
+        description: 'test text',
+      },
+      {
+        user: {
+          email: 'local-global-admin-user@kitos.dk',
+          name: 'Automatisk oprettet testbruger (GlobalAdmin)',
+        },
+        role: { name: 'Dataejer' },
+        writeAccess: false,
+        description: null,
+      },
+    ];
+
+    for (const expectedRow of expectedRows) {
+      const nameCell = cy.contains(expectedRow.role.name);
+      const row = () => nameCell.parentsUntil('tr').parent();
+      row().contains(expectedRow.user.email);
+      row().contains(expectedRow.user.name);
+      row().contains(expectedRow.writeAccess ? 'Ja' : 'Nej');
+
+      if (expectedRow.description) {
+        row()
+          .get('td')
+          .first()
+          .within(() => {
+            cy.get('app-tooltip').should('have.attr', 'ng-reflect-text', expectedRow.description);
+          });
+      }
+    }
+  });
+
+  it('can show empty System roles', () => {
+    cy.intercept('/api/v2/it-system-usages/*', { fixture: 'it-system-usage.json' });
+
+    cy.contains('System 3').click();
+
+    cy.intercept('/api/v2/it-system-usage-role-types*', []);
+    cy.intercept('/api/v2/**/roles', []);
+
+    cy.navigateToDetailsSubPage('Systemroller');
+
+    cy.contains('Ingen systemroller tilføjet endnu');
+    cy.contains('Tilføj systemroller');
+  });
+
+  it('can add new System role', () => {
+    cy.intercept('/api/v2/it-system-usages/*', { fixture: 'it-system-usage.json' });
+
+    cy.contains('System 3').click();
+
+    cy.intercept('/api/v2/it-system-usage-role-types*', { fixture: 'it-system-usage-available-roles.json' });
+    cy.intercept('/api/v2/**/roles', { fixture: 'it-system-usage-roles.json' });
+
+    cy.navigateToDetailsSubPage('Systemroller');
+
+    cy.intercept('/api/v2/**/users*', { fixture: 'users.json' });
+    cy.contains('Tilføj systemroller').click();
+
+    //select user from the dropdown
+    cy.dropdown('Vælg bruger', 'Automatisk oprettet testbruger (GlobalAdmin)', true);
+
+    //select role from the dropdown
+    cy.dropdown('Vælg rolle', 'TestRole', true);
+
+    //validate can click the 'save' button
+    cy.intercept('/api/v2/it-system-usages/**/add', {});
+    cy.get('app-dialog').contains('Tilføj').click();
+  });
 });
