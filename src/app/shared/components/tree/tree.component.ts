fc6a0e7c1a0a70d085e8b0dedb966707eb0ccf05
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2023-04-20 11:59:04 +0200

    Added toggle for status. Updated styling. Removed underline on anchors

--- src/app/shared/components/tree/tree.component.ts
+++ src/app/shared/components/tree/tree.component.ts
@@ -1,5 +1,5 @@
 import { NestedTreeControl } from '@angular/cdk/tree';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { MatTreeNestedDataSource } from '@angular/material/tree';
 import { RegistrationEntityTypes } from '../../models/registrations/registration-entity-categories.model';
 import { EntityTreeNode } from '../../models/structure/entity-tree-node.model';
@@ -9,10 +9,12 @@ import { EntityTreeNode } from '../../models/structure/entity-tree-node.model';
   templateUrl: './tree.component.html',
   styleUrls: ['./tree.component.scss'],
 })
-export class TreeComponent<T> {
+export class TreeComponent<T> implements OnInit {
   public readonly treeControl = new NestedTreeControl<EntityTreeNode<T>>((node) => node.children);
   public readonly dataSource = new MatTreeNestedDataSource<EntityTreeNode<T>>();
+  public toggleStatusText = 'status';
 
+  @Input() public showStatus = false;
   @Input() public allowExpandCollapse = true;
   @Input() public itemType!: RegistrationEntityTypes;
   @Input() public currentNodeUuid?: string;
@@ -23,4 +25,18 @@ export class TreeComponent<T> {
   }
 
   public readonly hasChild = (_: number, node: EntityTreeNode<T>) => !!node.children && node.children.length > 0;
+
+  ngOnInit(): void {
+    switch (this.itemType) {
+      case 'it-system-usage':
+      case 'it-system':
+        this.toggleStatusText = $localize`Vis tilgængelighed`;
+        break;
+      case 'it-contract':
+        this.toggleStatusText = $localize`Vis gyldighed`;
+        break;
+      default:
+        throw 'Unsupported item type:' + this.itemType;
+    }
+  }
 }
