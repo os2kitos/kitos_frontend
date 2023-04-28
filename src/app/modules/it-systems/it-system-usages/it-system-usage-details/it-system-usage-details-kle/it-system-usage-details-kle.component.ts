1b7a9b39f48192ad195cb0d5f860456bfa62cac4
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2023-04-28 10:09:25 +0200

    fix loading state on kle

--- src/app/modules/it-systems/it-system-usages/it-system-usage-details/it-system-usage-details-kle/it-system-usage-details-kle.component.ts
+++ src/app/modules/it-systems/it-system-usages/it-system-usage-details/it-system-usage-details-kle/it-system-usage-details-kle.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { combineLatest, map } from 'rxjs';
 import { BaseComponent } from 'src/app/shared/base/base.component';
 import { filterNullish } from 'src/app/shared/pipes/filter-nullish';
+import { invertBooleanValue } from 'src/app/shared/pipes/invert-boolean-value';
 import { matchNonEmptyArray } from 'src/app/shared/pipes/match-non-empty-array';
 import { selectItSystemKleWithDetails, selectItSystemLoading } from 'src/app/store/it-system/selectors';
 import { KLEActions } from 'src/app/store/kle/actions';
+import { selectHasValidCache } from 'src/app/store/kle/selectors';
 
 @Component({
   selector: 'app-it-system-usage-details-kle',
@@ -12,11 +15,14 @@ import { KLEActions } from 'src/app/store/kle/actions';
   styleUrls: ['./it-system-usage-details-kle.component.scss'],
 })
 export class ItSystemUsageDetailsKleComponent extends BaseComponent implements OnInit {
-  public readonly inheritedkle$ = this.store.select(selectItSystemKleWithDetails).pipe(filterNullish());
-  public readonly loadingInerited$ = this.store.select(selectItSystemLoading).pipe(filterNullish());
+  private readonly loadingSystemContext$ = this.store.select(selectItSystemLoading);
+  private readonly loadingKle$ = this.store.select(selectHasValidCache).pipe(invertBooleanValue());
+  public readonly systemContextKleDetails$ = this.store.select(selectItSystemKleWithDetails).pipe(filterNullish());
+  public readonly loadingSystemContextKle$ = combineLatest([this.loadingSystemContext$, this.loadingKle$]).pipe(
+    map(([loadingInerited, loadingKleDetails]) => loadingInerited || loadingKleDetails)
+  );
 
-  //TODO: Also check on the state of loading kle store
-  public readonly anyInherited$ = this.inheritedkle$.pipe(filterNullish(), matchNonEmptyArray());
+  public readonly anyInherited$ = this.systemContextKleDetails$.pipe(filterNullish(), matchNonEmptyArray());
 
   constructor(private readonly store: Store) {
     super();
