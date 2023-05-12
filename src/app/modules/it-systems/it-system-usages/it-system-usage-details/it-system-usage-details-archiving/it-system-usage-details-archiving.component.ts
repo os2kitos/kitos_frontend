d50430dfe8f31fd1e96f8243c2c36c49c134c00d
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2023-05-12 10:59:49 +0200

    - updated translations - removed demo sections

--- src/app/modules/it-systems/it-system-usages/it-system-usage-details/it-system-usage-details-archiving/it-system-usage-details-archiving.component.ts
+++ src/app/modules/it-systems/it-system-usages/it-system-usage-details/it-system-usage-details-archiving/it-system-usage-details-archiving.component.ts
@@ -1,33 +1,9 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
 import { BaseComponent } from 'src/app/shared/base/base.component';
-import { RadioButtonOption } from 'src/app/shared/components/radio-buttons/radio-buttons.component';
 
 @Component({
   selector: 'app-it-system-usage-details-archiving',
   templateUrl: './it-system-usage-details-archiving.component.html',
   styleUrls: ['./it-system-usage-details-archiving.component.scss'],
 })
-export class ItSystemUsageDetailsArchivingComponent extends BaseComponent {
-  public readonly testForm = new FormGroup(
-    {
-      archivingActive: new FormControl<string | undefined>('2'),
-    },
-    { updateOn: 'blur' }
-  );
-
-  public readonly options: Array<RadioButtonOption> = [
-    {
-      label: 'Ja',
-      uuid: '1',
-    },
-    {
-      label: 'Nej',
-      uuid: '2',
-    },
-  ];
-
-  public changeArchivingActive(value?: string) {
-    console.log(value);
-  }
-}
+export class ItSystemUsageDetailsArchivingComponent extends BaseComponent {}
