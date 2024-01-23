aae0bcddd68768ef55eee19a6b12fab20816b3c0
NikolajNielsen <nan@strongminds.dk>
2024-01-23 07:59:05 +0100

    Cleanup imports

--- src/app/modules/it-systems/it-system-usages/it-system-usage-details/it-system-usage-details-gdpr/it-system-usage-details-gdpr.component.ts
+++ src/app/modules/it-systems/it-system-usages/it-system-usage-details/it-system-usage-details-gdpr/it-system-usage-details-gdpr.component.ts
@@ -1,17 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { BaseComponent } from 'src/app/shared/base/base.component';
-import { HostedAt, hostedAtOptions, mapHostedAt } from 'src/app/shared/models/gdpr/hosted-at.model';
 import { filterNullish } from 'src/app/shared/pipes/filter-nullish';
-import { selectItSystemUsageGdpr } from 'src/app/store/it-system-usage/selectors';
-import { RegularOptionTypeActions } from 'src/app/store/regular-option-type-store/actions';
 import { selectRegularOptionTypes } from 'src/app/store/regular-option-type-store/selectors';
-import { BusinessCritical, businessCriticalOptions, mapBusinessCritical } from '../../../../../shared/models/gdpr/business-critical.model';
-import { APIGDPRWriteRequestDTO } from 'src/app/api/v2';
-import { ValidatedValueChange } from 'src/app/shared/models/validated-value-change.model';
-import { NotificationService } from 'src/app/shared/services/notification.service';
-import { ITSystemUsageActions } from 'src/app/store/it-system-usage/actions';
 
 @Component({
   selector: 'app-it-system-usage-details-gdpr',
