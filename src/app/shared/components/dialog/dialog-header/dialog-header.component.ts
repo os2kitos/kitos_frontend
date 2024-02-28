fdc3556fa38d9408e0ce7051a22461c788c0a168
Aleksander Naskret <aln@strongminds.dk>
2024-02-28 09:39:46 +0100

    merge

--- src/app/shared/components/dialog/dialog-header/dialog-header.component.ts
+++ src/app/shared/components/dialog/dialog-header/dialog-header.component.ts
@@ -1,11 +1,11 @@
 import { Component, Input } from '@angular/core';
-import { DialogComponent } from '../dialog.component';
 import { MatDialogRef } from '@angular/material/dialog';
+import { DialogComponent } from '../../dialogs/dialog/dialog.component';
 
 @Component({
   selector: 'app-dialog-header',
   templateUrl: './dialog-header.component.html',
-  styleUrl: './dialog-header.component.scss'
+  styleUrl: './dialog-header.component.scss',
 })
 export class DialogHeaderComponent {
   @Input() public title?: string;
--- src/app/shared/components/dialog/scrollbar-dialog/scrollbar-dialog.component.ts
+++ src/app/shared/components/dialog/scrollbar-dialog/scrollbar-dialog.component.ts
@@ -1,11 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
-import { DialogComponent } from '../dialog.component';
+import { DialogComponent } from '../../dialogs/dialog/dialog.component';
 
 @Component({
   selector: 'app-scrollbar-dialog',
   templateUrl: './scrollbar-dialog.component.html',
-  styleUrl: './scrollbar-dialog.component.scss'
+  styleUrl: './scrollbar-dialog.component.scss',
 })
 export class ScrollbarDialogComponent {
   @Input() public title?: string;
