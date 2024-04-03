6f172605a27a5d7dcbfd2c2b17026638218709df
Morten Hoffmann Sørensen <mhs@strongminds.dk>
2024-04-03 12:15:00 +0200

    KITOSUDV-4920 Bug in form validity

--- src/app/shared/components/notifications-table/notifications-table-dialog/notifications-table-dialog.component.ts
+++ src/app/shared/components/notifications-table/notifications-table-dialog/notifications-table-dialog.component.ts
@@ -4,7 +4,7 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { distinctUntilChanged } from 'rxjs';
 import { APIEmailRecipientResponseDTO, APINotificationResponseDTO, APIRegularOptionResponseDTO, APIRoleRecipientResponseDTO } from 'src/app/api/v2';
 import { NOTIFICATIONS_DIALOG_DEFAULT_WIDTH } from 'src/app/shared/constants';
-import { checkboxesCheckedValidator, dateGreaterThanControlValidator, dateLessThanOrEqualToDateValidator } from 'src/app/shared/helpers/form.helpers';
+import { atLeastOneCheckboxCheckedValidator, atLeastOneNonEmptyValidator, dateGreaterThanControlValidator, dateLessThanOrEqualToDateValidator } from 'src/app/shared/helpers/form.helpers';
 import { NotificationRepetitionFrequency, mapNotificationRepetitionFrequency } from 'src/app/shared/models/notification-repetition-frequency.model';
 import { NotificationType, mapNotificationType, notificationTypeOptions } from 'src/app/shared/models/notification-type.model';
 import { ValidatedValueChange } from 'src/app/shared/models/validated-value-change.model';
@@ -28,7 +28,7 @@ export class NotificationsTableDialogComponent implements OnInit {
   @Input() public confirmText!: string;
 
   public readonly notificationForm = new FormGroup({
-    emailRecipientsFormArray: new FormArray([new FormControl<string | undefined>(undefined, Validators.email)]),
+    emailRecipientsFormArray: new FormArray([new FormControl<string | undefined>(undefined, [Validators.email, Validators.required])]),
     emailCcsFormArray: new FormArray([new FormControl<string | undefined>(undefined, Validators.email)]),
     subjectControl: new FormControl<string | undefined>(undefined, Validators.required),
     notificationTypeControl: new FormControl<NotificationType | undefined>(undefined, Validators.required),
@@ -39,7 +39,7 @@ export class NotificationsTableDialogComponent implements OnInit {
     bodyControl: new FormControl<string | undefined>(undefined, Validators.required)
   });
 
-  public readonly roleRecipientsForm = new FormGroup({}, checkboxesCheckedValidator());
+  public readonly roleRecipientsForm = new FormGroup({});
   public readonly roleCcsForm = new FormGroup({});
 
   public readonly notificationTypeOptions = notificationTypeOptions;
@@ -62,18 +62,17 @@ export class NotificationsTableDialogComponent implements OnInit {
     private readonly dialogRef: MatDialogRef<NotificationsTableDialogComponent>,
     private readonly componentStore: NotificationsTableComponentStore,
     @Inject(MAT_DIALOG_DATA) public data: APINotificationResponseDTO
-    )
-    {
-      dialogRef.updateSize(`${NOTIFICATIONS_DIALOG_DEFAULT_WIDTH}px`);
-      if (data) this.notification = data;
-    }
+  ) {
+    dialogRef.updateSize(`${NOTIFICATIONS_DIALOG_DEFAULT_WIDTH}px`);
+    if (data) this.notification = data;
+  }
 
   ngOnInit(): void {
     this.setupNotificationControls();
     this.setupRoleRecipientControls();
-    this.setupRecipientFieldsRelationship();
+    //this.setupRecipientFieldsRelationship();
 
-    if (this.hasImmediateNotification()){
+    if (this.hasImmediateNotification()) {
       this.notificationForm.disable();
       this.roleRecipientsForm.disable();
       this.roleCcsForm.disable();
@@ -81,61 +80,21 @@ export class NotificationsTableDialogComponent implements OnInit {
       this.emailCcsFormArray.controls.forEach((control) => control.disable());
     }
 
-    if (this.notification && this.notification.notificationType === this.notificationTypeRepeat.value)
-    {
+    if (this.notification && this.notification.notificationType === this.notificationTypeRepeat.value) {
       this.toggleRepetitionFields(true);
       this.setupSentTable();
     }
     else this.toggleRepetitionFields(false);
   }
 
-  private setupSentTable() {
-    if (this.notification?.uuid) this.componentStore.getCurrentNotificationSent({
-      ownerResourceUuid: this.ownerEntityUuid,
-      notificationUuid: this.notification.uuid
-    })
-  }
-
-  private hasImmediateNotification = () =>
-    this.notification && this.notification.notificationType !== this.notificationTypeRepeat.value;
-
-  public handleClickConfirm(){
-    this.onConfirm(this.notificationForm, this.roleRecipientsForm, this.roleCcsForm, this.emailRecipientsFormArray, this.emailCcsFormArray);
-  }
-
-  private setupRecipientFieldsRelationship(){
-    this.emailRecipientsFormArray.valueChanges
-      .pipe(distinctUntilChanged((a, b) => this.compareAsJson(a, b)))
-      .subscribe(arrayValue => {
-        if (arrayValue.some((value: string) => value !== '')) this.roleRecipientsForm.clearValidators();
-        else this.roleRecipientsForm.setValidators(checkboxesCheckedValidator());
-        this.roleRecipientsForm.updateValueAndValidity();
-      });
-
-    this.roleRecipientsForm.valueChanges
-      .pipe(distinctUntilChanged((a, b) => this.compareAsJson(a, b)))
-      .subscribe(values => {
-      if (Object.values(values).some(value => value)) this.emailRecipientsFormArray.controls.forEach((control => control.setValidators(Validators.email)));
-      else this.emailRecipientsFormArray.controls.forEach((control => control.setValidators([Validators.email, Validators.required])));
-      this.notificationForm.updateValueAndValidity();
-    });
-  }
-
-  public formatDate(date: string | undefined) {
-    if (date) return new Date(date).toLocaleString()
-    return $localize`Ugyldig dato fundet.`
-  }
-
-  private setupNotificationControls(){
+  private setupNotificationControls() {
     const notificationControls = this.notificationForm.controls;
     notificationControls.fromDateControl.addValidators(dateLessThanOrEqualToDateValidator(new Date()));
     notificationControls.toDateControl.validator = dateGreaterThanControlValidator(this.notificationForm.controls.fromDateControl);
-    notificationControls.fromDateControl.valueChanges.subscribe(() =>
-      this.toggleShowDateOver28Tooltip());
-    notificationControls.repetitionControl.valueChanges.subscribe(() =>
-      this.toggleShowDateOver28Tooltip());
+    notificationControls.fromDateControl.valueChanges.subscribe(() => this.toggleShowDateOver28Tooltip());
+    notificationControls.repetitionControl.valueChanges.subscribe(() => this.toggleShowDateOver28Tooltip());
 
-    if (this.notification){
+    if (this.notification) {
       const fromDate = this.notification.fromDate;
       if (fromDate) notificationControls.fromDateControl.setValue(new Date(fromDate));
       const toDate = this.notification.toDate;
@@ -149,43 +108,82 @@ export class NotificationsTableDialogComponent implements OnInit {
       this.setupEmailRecipientData(this.notification.receivers?.emailRecipients, this.emailRecipientsFormArray);
       this.setupEmailRecipientData(this.notification.cCs?.emailRecipients, this.emailCcsFormArray);
     }
+    this.emailRecipientsFormArray.addValidators(atLeastOneNonEmptyValidator);
+    this.emailRecipientsFormArray.updateValueAndValidity();
   }
 
-  private setupEmailRecipientData(recipients: APIEmailRecipientResponseDTO[] | undefined, formArray: FormArray){
-    const lastControl = formArray.controls[formArray.controls.length -1];
-    recipients?.forEach((recipient) =>
-      {
-        if (!lastControl.value) lastControl.setValue(recipient.email);
-        else formArray.controls.push(new FormControl<string | undefined>(recipient.email));
-      })
+  private setupEmailRecipientData(recipients: APIEmailRecipientResponseDTO[] | undefined, formArray: FormArray) {
+    const lastControl = formArray.controls[formArray.controls.length - 1];
+    recipients?.forEach((recipient) => {
+      if (!lastControl.value) lastControl.setValue(recipient.email);
+      else formArray.controls.push(new FormControl<string | undefined>(recipient.email, [Validators.email, Validators.required]));
+    })
   }
 
-  private setupRoleRecipientControls(){
+  private setupRoleRecipientControls() {
     this.systemUsageRolesOptions.forEach((option) => {
       this.roleRecipientsForm.addControl(option.uuid, new FormControl<boolean>(false));
       this.roleCcsForm.addControl(option.uuid, new FormControl<boolean>(false));
     })
-    if (this.notification){
+    if (this.notification) {
       this.setupRoleRecipientData(this.notification.receivers?.roleRecipients, this.roleRecipientsForm);
       this.setupRoleRecipientData(this.notification.cCs?.roleRecipients, this.roleCcsForm);
     }
+    this.roleRecipientsForm.setValidators(atLeastOneCheckboxCheckedValidator);
+    this.roleRecipientsForm.updateValueAndValidity();
   }
 
-  private setupRoleRecipientData(recipients: APIRoleRecipientResponseDTO[] | undefined, form: FormGroup){
+  private setupRoleRecipientData(recipients: APIRoleRecipientResponseDTO[] | undefined, form: FormGroup) {
     recipients?.forEach((recipient) => {
-    for (const controlKey in form.controls){
-      if (controlKey === recipient.role?.uuid){
-        const control = form.get(controlKey);
-        control?.setValue(true);
+      for (const controlKey in form.controls) {
+        if (controlKey === recipient.role?.uuid) {
+          const control = form.get(controlKey);
+          control?.setValue(true);
+        }
       }
-    }})
+    })
+  }
+
+  private setupRecipientFieldsRelationship() {
+    this.emailRecipientsFormArray.valueChanges
+      .pipe(distinctUntilChanged((a, b) => this.compareAsJson(a, b)))
+      .subscribe(() => {
+        this.roleRecipientsForm.updateValueAndValidity();
+      });
+
+    this.roleRecipientsForm.valueChanges
+      .pipe(distinctUntilChanged((a, b) => this.compareAsJson(a, b)))
+      .subscribe(() => {
+        this.emailRecipientsFormArray.updateValueAndValidity();
+      });
+  }
+
+  private compareAsJson = (a: unknown, b: unknown): boolean => JSON.stringify(a) === JSON.stringify(b)
+
+  public formatDate(date: string | undefined) {
+    if (date) return new Date(date).toLocaleString()
+    return $localize`Ugyldig dato fundet.`
+  }
+
+  private setupSentTable() {
+    if (this.notification?.uuid) this.componentStore.getCurrentNotificationSent({
+      ownerResourceUuid: this.ownerEntityUuid,
+      notificationUuid: this.notification.uuid
+    })
+  }
+
+  private hasImmediateNotification = () =>
+    this.notification && this.notification.notificationType !== this.notificationTypeRepeat.value;
+
+  public handleClickConfirm() {
+    this.onConfirm(this.notificationForm, this.roleRecipientsForm, this.roleCcsForm, this.emailRecipientsFormArray, this.emailCcsFormArray);
   }
 
   public changeNotificationType(newValue: string, valueChange?: ValidatedValueChange<unknown>) {
     if (valueChange && !valueChange.valid) {
       this.notificationService.showError($localize`"${valueChange.text}" er ugyldig`);
     } else {
-        this.toggleRepetitionFields(newValue === this.notificationTypeRepeat.value)
+      this.toggleRepetitionFields(newValue === this.notificationTypeRepeat.value)
     }
   }
 
@@ -193,31 +191,28 @@ export class NotificationsTableDialogComponent implements OnInit {
     this.dialogRef.close();
   }
 
-  public repeatIsSelected(){
+  public repeatIsSelected() {
     return this.notificationForm.controls.notificationTypeControl.value === this.notificationTypeRepeat;
   }
 
-  public onAddEmailField(formArrayName: string){
+  public onAddEmailField(formArrayName: string) {
     const formArray = this.notificationForm.get(formArrayName) as FormArray;
     formArray.controls.push(new FormControl<string | undefined>(undefined, Validators.email));
   }
 
-  public onRemoveEmailField(index: number, formArrayName: string){
+  public onRemoveEmailField(index: number, formArrayName: string) {
     const formArray = this.notificationForm.get(formArrayName) as FormArray;
-    if (formArray.controls.length > 1 && !this.hasImmediateNotification()){
+    if (formArray.controls.length > 1 && !this.hasImmediateNotification()) {
       formArray.controls.splice(index, 1);
     }
   }
 
-  private compareAsJson = (a: unknown, b: unknown): boolean =>
-    JSON.stringify(a) === JSON.stringify(b)
-
   private toggleRepetitionFields(isRepeated: boolean) {
     const allControls = this.notificationForm.controls;
     const toBeToggled = [allControls.nameControl,
-      allControls.repetitionControl,
-      allControls.fromDateControl,
-      allControls.toDateControl]
+    allControls.repetitionControl,
+    allControls.fromDateControl,
+    allControls.toDateControl]
     if (isRepeated) {
       toBeToggled.forEach((control) => control.enable());
     } else {
@@ -230,11 +225,11 @@ export class NotificationsTableDialogComponent implements OnInit {
   private toggleShowDateOver28Tooltip() {
     const notificationControls = this.notificationForm.controls;
     const fromDate = notificationControls.fromDateControl.value;
-    if (fromDate){
+    if (fromDate) {
       const dayOfMonth = new Date(fromDate).getDate();
       const repetition = notificationControls.repetitionControl.value;
       const repetitionIsMonthOrMore =
-      this.notificationRepetitionFrequencyOptions.findIndex((option) => option.value === repetition?.value)
+        this.notificationRepetitionFrequencyOptions.findIndex((option) => option.value === repetition?.value)
         > 2;
 
       this.showDateOver28Tooltip = dayOfMonth > 28 && repetitionIsMonthOrMore;
--- src/app/shared/helpers/form.helpers.ts
+++ src/app/shared/helpers/form.helpers.ts
@@ -1,4 +1,4 @@
-import { AbstractControl, FormGroup, ValidationErrors, ValidatorFn } from '@angular/forms';
+import { AbstractControl, FormArray, FormGroup, ValidationErrors, ValidatorFn } from '@angular/forms';
 import * as moment from 'moment';
 
 interface OnInvalid {
@@ -16,21 +16,21 @@ function toDate(input: unknown): Date | undefined {
 export function dateGreaterThanControlValidator(startControl: AbstractControl): ValidatorFn {
   return (endControl: AbstractControl): ValidationErrors | null => {
     return compareControlDates(
-       startControl,
-       endControl,
-       (a: Date, b: Date) => a.setHours(0, 0, 0, 0) > b.setHours(0, 0, 0, 0),
-       { greaterThan: true })
-    }
+      startControl,
+      endControl,
+      (a: Date, b: Date) => a.setHours(0, 0, 0, 0) > b.setHours(0, 0, 0, 0),
+      { greaterThan: true })
+  }
 }
 
 export function dateLessThanControlValidator(startControl: AbstractControl): ValidatorFn {
   return (endControl: AbstractControl): ValidationErrors | null => {
     return compareControlDates(
-       startControl,
-       endControl,
-       (a: Date, b: Date) => a.setHours(0, 0, 0, 0) < b.setHours(0, 0, 0, 0),
-       { lessThan: true })
-    }
+      startControl,
+      endControl,
+      (a: Date, b: Date) => a.setHours(0, 0, 0, 0) < b.setHours(0, 0, 0, 0),
+      { lessThan: true })
+  }
 }
 
 export function dateLessThanOrEqualToDateValidator(startDate: Date): ValidatorFn {
@@ -45,29 +45,34 @@ export function dateLessThanOrEqualToDateValidator(startDate: Date): ValidatorFn
   }
 }
 
+export function atLeastOneCheckboxCheckedValidator(formGroup: AbstractControl): ValidationErrors | null {
+  if (formGroup instanceof FormGroup) {
+    const controls = formGroup.controls;
+    const isAtLeastOneChecked = Object.keys(controls).some(key => controls[key].value === true);
+    return isAtLeastOneChecked ? null : { 'atLeastOneRequired': true };
+  }
+  return null;
+}
+
+export function atLeastOneNonEmptyValidator(formArray: AbstractControl): ValidationErrors | null {
+  if (formArray instanceof FormArray) {
+    const controls = formArray.controls;
+    const validControls = controls.filter(ctrl => ctrl.valid && ctrl.value != null);
+    return validControls.length > 0 ? null : { 'atLeastOneValid': true };
+  }
+  return null;
+}
+
 function compareControlDates(startControl: AbstractControl, endControl: AbstractControl,
-  comparator: (a: Date, b: Date) => boolean, onInvalid: OnInvalid){
-    const startDate = toDate(startControl.value);
-    const endDate = toDate(endControl.value);
-    return compareDates(startDate, endDate, comparator, onInvalid);
+  comparator: (a: Date, b: Date) => boolean, onInvalid: OnInvalid) {
+  const startDate = toDate(startControl.value);
+  const endDate = toDate(endControl.value);
+  return compareDates(startDate, endDate, comparator, onInvalid);
 }
 
 function compareDates(startDate: Date | undefined, endDate: Date | undefined,
-  comparator: (a: Date, b: Date) => boolean, onInvalid: OnInvalid){
-    if (!startDate || !endDate) return null;
-    return comparator(startDate, endDate) ? onInvalid : null;
+  comparator: (a: Date, b: Date) => boolean, onInvalid: OnInvalid) {
+  if (!startDate || !endDate) return null;
+  return comparator(startDate, endDate) ? onInvalid : null;
 }
 
-export function checkboxesCheckedValidator(numRequired = 1): ValidatorFn {
-  return (formGroup: AbstractControl): ValidationErrors | null => {
-    if (formGroup instanceof FormGroup){
-      let numChecked = 0;
-      Object.keys(formGroup.controls).forEach((key) => {
-        const control = formGroup.controls[key];
-        if (control.value) numChecked++;
-      })
-      return numChecked >= numRequired ? null : { CheckboxesChecked: true };
-    }
-    return null;
-  }
-}
