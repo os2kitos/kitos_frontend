9bdac5aed76964e13ff3ffeb59eb35da4eabcc6e
Aleksander Naskret <aln@strongminds.dk>
2024-04-05 13:57:31 +0200

    dpr table

--- src/app/modules/it-contracts/it-contract-details/it-contract-dpr/it-contract-dpr.component.ts
+++ src/app/modules/it-contracts/it-contract-details/it-contract-dpr/it-contract-dpr.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { BaseComponent } from 'src/app/shared/base/base.component';
+import { filterNullish } from 'src/app/shared/pipes/filter-nullish';
+import { matchNonEmptyArray } from 'src/app/shared/pipes/match-non-empty-array';
+import { selectItContractDataProcessingRegistrations } from 'src/app/store/it-contract/selectors';
 
 @Component({
   selector: 'app-it-contract-dpr',
   templateUrl: './it-contract-dpr.component.html',
-  styleUrl: './it-contract-dpr.component.scss'
+  styleUrl: './it-contract-dpr.component.scss',
 })
-export class ItContractDprComponent {
+export class ItContractDprComponent extends BaseComponent {
+  public readonly dataProcessingRegistrations$ = this.store
+    .select(selectItContractDataProcessingRegistrations)
+    .pipe(filterNullish());
+  public readonly anyDataProcessingRegistrations$ = this.dataProcessingRegistrations$.pipe(matchNonEmptyArray());
 
+  constructor(private readonly store: Store) {
+    super();
+  }
+
+  public onDelete(uuid: string): void {}
+
+  public onAdd(): void {}
 }
