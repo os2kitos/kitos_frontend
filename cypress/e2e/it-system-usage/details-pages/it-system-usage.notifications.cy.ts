e8336b6a7df28fcfddea8a46c0411065aebeae61
Morten Hoffmann Sørensen <mhs@strongminds.dk>
2024-04-12 18:34:49 +0200

    Fixed cypress tests

--- cypress/e2e/it-system-usage/details-pages/it-system-usage.notifications.cy.ts
+++ cypress/e2e/it-system-usage/details-pages/it-system-usage.notifications.cy.ts
@@ -61,56 +61,59 @@ describe('it-system-usage', () => {
       });
   });
 
-  /*
   it('Can deactivate and delete notifications', () => {
 
     cy.getRowForElementContent('test2')
       .first()
       .within(() => {
         cy.getByDataCy('deactivate-button').click();
-        })
+      })
     cy.getByDataCy('confirm-dialog').within(() => {
-    cy.intercept('PATCH', '/api/v2/internal/notifications/ItSystemUsage/85cc6bd8-da66-4d4e-8e56-4dbc16e5c109/scheduled/deactivate/310ea75c-1111-4bcd-1111-a19543b0dcc5', { fixture: './it-system-usage/notifications/notifications-deactivate.json'} ).as('patch');
-
-    cy.getByDataCy('confirm-button').click()
-    cy.verifyRequestUsingDeepEq('patch', 'request.body', {uuid: '310ea75c-1111-4bcd-1111-a19543b0dcc5'})
-      });
+      cy.intercept('PATCH', '/api/v2/internal/notifications/ItSystemUsage/85cc6bd8-da66-4d4e-8e56-4dbc16e5c109/scheduled/deactivate/310ea75c-1111-4bcd-1111-a19543b0dcc5',
+        { fixture: './it-system-usage/notifications/notifications-deactivate.json' })
+        .as('patch')
+      cy.getByDataCy('confirm-button').click()
+      cy.verifyRequestUsingDeepEq('patch', 'response.body.uuid', '310ea75c-1111-4bcd-1111-a19543b0dcc5')
+    });
   })
-  */
 
-/*
   it('Can add notification', () => {
     const recipient = 'test@test.dk';
     const subject = 'testSubject';
     const body = 'testBody';
 
     cy.intercept('POST', '/api/v2/internal/notifications/*', {
-    fixture: './it-system-usage/notifications/it-system-usage-post-notification.json',
+      fixture: './it-system-usage/notifications/it-system-usage-post-notification.json',
     }).as('postNewNotification')
-
     cy.getByDataCy('add-notification-button').click();
 
     cy.getByDataCy('notifications-dialog').within(() => {
-
       cy.getByDataCy('email-recipient-accordion').click().getByDataCy('email-recipient-textbox').type(recipient);
       cy.dropdownByCy('notification-type-dropdown', 'Straks', true)
       cy.getByDataCy('subject-textbox').type('testSubject')
       cy.setTinyMceContent('body-editor', 'testBody')
-      cy.getIframe().click({force: true})
+      cy.getIframe().click({ force: true })
       //Clicks twice to first defocus body editor and update form value, then save the notification.
       //This is required because the Cypress interacts with the editor by "setting" content rather than typing.
       cy.getByDataCy('confirm-button').click()
+
+      cy.intercept('POST', '/api/v2/internal/notifications/ItSystemUsage/85cc6bd8-da66-4d4e-8e56-4dbc16e5c109/immediate',
+        { fixture: './it-system-usage/notifications/it-system-usage-post-notification.json' })
+        .as('postNewNotification')
       cy.getByDataCy('confirm-button').click()
 
-      const expectedProperties = {receivers: {emailRecipients: [recipient]},
+      const expectedProperties = {
+        receivers: { emailRecipients: [recipient] },
         subject: subject,
         body: body,
         notificationType: "Immediate"
       }
-      cy.wait('@postNewNotification').its('body').should('deep.eq', expectedProperties)
+      cy.wait('@postNewNotification').its('response.body').then(body => {
+        expect(body.notificationType).to.deep.eq(expectedProperties.notificationType)
+        expect(body.subject).to.deep.eq(expectedProperties.subject)
+        expect(body.body).to.deep.eq(expectedProperties.body)
+        expect(body.receivers.emailRecipients).to.deep.eq(expectedProperties.receivers.emailRecipients)
+      });
     });
-
-
   });
-*/
 });
