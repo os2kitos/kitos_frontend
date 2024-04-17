a894f36f342bcc039622a06f731346719557690b
Aleksander Naskret <aln@strongminds.dk>
2024-04-17 07:48:01 +0200

    test fix

---
+++ cypress/e2e/it-contracts/details-pages/it-contract.external-references.cy.ts
@@ -0,0 +1,88 @@
+/// <reference types="Cypress" />
+
+describe('it-contracts', () => {
+  const itContractBaseUrl = '/api/v2/it-contracts/*';
+  beforeEach(() => {
+    cy.requireIntercept();
+    cy.intercept('/odata/ItContract*', { fixture: './it-contracts/it-contracts.json' });
+    cy.intercept('/api/v2/it-contracts/*', { fixture: './it-contracts/it-contract.json' });
+    cy.intercept('/api/v2/organizations/*/users', { fixture: './organizations/organization-users.json' });
+    cy.intercept('/api/v2/organizations?orderByProperty*', { fixture: './organizations/organizations-multiple.json' });
+    cy.intercept('/api/v2/organizations/*/organization-units*', {
+      fixture: './organizations/organization-units-hierarchy.json',
+    });
+    cy.intercept('/api/v2/it-contract-contract-types*', { fixture: './it-contracts/choice-types/contract-types.json' });
+    cy.intercept('/api/v2/it-contract-contract-template-types*', {
+      fixture: './it-contracts/choice-types/contract-templates.json',
+    });
+    cy.intercept('/api/v2/it-contract-criticality-types*', {
+      fixture: './it-contracts/choice-types/criticality-types.json',
+    });
+    cy.intercept('/api/v2/it-contract-procurement-strategy-types*', {
+      fixture: './it-contracts/choice-types/procurement-strategies.json',
+    });
+    cy.intercept('/api/v2/it-contract-purchase-types*', { fixture: './it-contracts/choice-types/purchase-types.json' });
+    cy.setup(true, 'it-contracts');
+  });
+
+  it('can show external references', () => {
+    cy.intercept('/api/v2/it-contracts/*', {
+      fixture: './it-contracts/it-contract-with-extra-external-reference.json',
+    });
+    cy.contains('Contract 1').click();
+
+    cy.navigateToDetailsSubPage('Referencer');
+
+    cy.testCanShowExternalRefernces();
+  });
+
+  it('can show no external references', () => {
+    cy.contains('Contract').click();
+    cy.navigateToDetailsSubPage('Referencer');
+
+    cy.contains('Der er endnu ikke tilføjet referencer');
+    cy.contains('Opret reference');
+  });
+
+  it('can add external reference with required master reference, when no reference is present', () => {
+    cy.contains('Contract 1').click();
+    cy.navigateToDetailsSubPage('Referencer');
+
+    cy.externalReferencesSaveAndValidate(
+      true,
+      false,
+      false,
+      itContractBaseUrl,
+      './it-contracts/it-contract-with-edited-extra-external-reference.json'
+    );
+  });
+
+  it('can modify external reference, and assign new Master reference', () => {
+    cy.intercept('/api/v2/it-contracts/*', {
+      fixture: './it-contracts/it-contract-with-extra-external-reference.json',
+    });
+    cy.contains('Contract 1').click();
+    cy.navigateToDetailsSubPage('Referencer');
+
+    cy.externalReferencesSaveAndValidate(
+      false,
+      true,
+      true,
+      itContractBaseUrl,
+      './it-contracts/it-contract-with-edited-extra-external-reference.json',
+      'Valid url'
+    );
+  });
+
+  it('can not delete master external reference', () => {
+    cy.intercept('/api/v2/it-contracts/*', {
+      fixture: './it-contracts/it-contract-with-extra-external-reference.json',
+    });
+    cy.contains('Contract 1').click();
+    cy.navigateToDetailsSubPage('Referencer');
+
+    cy.getRowForElementContent('No url Master reference')
+      .first()
+      .within(() => cy.get('app-trashcan-icon').should('not.exist'));
+  });
+});
