2220086f764dd59f9c1b7d52d7af9f9680ac1338
Morten Hoffmann Sørensen <mhs@strongminds.dk>
2024-04-30 15:43:16 +0200

    Skeleton for cypress notification tests

---
+++ cypress/e2e/it-contracts/details-pages/it-contracts.notifications.cy.ts
@@ -0,0 +1,36 @@
+describe('it-contract-notifications', () => {
+  beforeEach(() => {
+    cy.requireIntercept();
+    cy.intercept('/odata/ItContract*', { fixture: './it-contracts/it-contracts.json' });
+    cy.intercept('/api/v2/it-contracts/*', { fixture: './it-contracts/it-contract.json' });
+    cy.intercept('/api/v2/organizations/*/users', { fixture: './organizations/organization-users.json' });
+    cy.intercept('/api/v2/organizations?orderByProperty*', { fixture: './organizations/organizations-multiple.json' });
+    cy.intercept('/api/v2/organizations/*/organization-units*', {
+      fixture: './organizations/organization-units-hierarchy.json',
+    });
+    cy.intercept('/api/v2/it-contract-contract-types*', { fixture: './it-contracts/choice-types/contract-types.json' });
+    cy.intercept('/api/v2/it-contract-contract-template-types*', {
+      fixture: './it-contracts/choice-types/contract-templates.json',
+    });
+    cy.intercept('/api/v2/it-contract-criticality-types*', {
+      fixture: './it-contracts/choice-types/criticality-types.json',
+    });
+    cy.intercept('/api/v2/it-contract-procurement-strategy-types*', {
+      fixture: './it-contracts/choice-types/procurement-strategies.json',
+    });
+    cy.intercept('/api/v2/it-contract-purchase-types*', { fixture: './it-contracts/choice-types/purchase-types.json' });
+    cy.intercept('/api/v2/it-contracts/*/permissions', { fixture: './it-contracts/it-contract-permissions.json' });
+    cy.intercept('api/v2/it-contracts?organizationUuid*', {
+      fixture: './it-contracts/it-contracts-by-it-system-usage-uuid.json',
+    });
+    cy.setup(true, 'it-contracts');
+    cy.contains('Contract 1').click();
+    cy.intercept('/api/v2/it-contracts/*/notifications', { fixture: './it-contracts/notifications/it-contract-notifications.json' });
+    cy.intercept('/api/v2/internal/notifications/ItContract*', { fixture: './it-contracts/notifications/it-contract-notification.json' });
+    cy.navigateToDetailsSubPage('Advis');
+  });
+
+  it('Sent notifications shown when eye icon clicked', () => {
+    cy.getByDataCy('notification-card-title').should('have.text', 'Advis');
+  });
+});
