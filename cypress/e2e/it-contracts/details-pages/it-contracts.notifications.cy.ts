31b54a73be2018d288e9c2707cc8e8a5f0c4fcd2
Morten Hoffmann Sørensen <mhs@strongminds.dk>
2024-05-01 13:40:42 +0200

    Added eye icon test

--- cypress/e2e/it-contracts/details-pages/it-contracts.notifications.cy.ts
+++ cypress/e2e/it-contracts/details-pages/it-contracts.notifications.cy.ts
@@ -27,6 +27,9 @@ describe('it-contract-notifications', () => {
     cy.intercept('/api/v2/internal/notifications/ItContract*', {
       fixture: './it-contracts/notifications/it-contract-notifications.json'
     });
+    cy.intercept('/api/v2/it-contract-role-types*', {
+      fixture: './it-contracts/notifications/it-contract-role-types.json',
+    });
 
     cy.contains('Contract 1').click();
     cy.navigateToDetailsSubPage('Advis');
@@ -34,5 +37,10 @@ describe('it-contract-notifications', () => {
 
   it('Sent notifications shown when eye icon clicked', () => {
     cy.getByDataCy('notification-card-title').should('have.text', 'Advis');
+    cy.intercept('/api/v2/internal/notifications/ItContract/*/sent/*', {
+      fixture: './it-contracts/notifications/it-contract-notification-sent.json',
+    });
+    cy.getByDataCy('view-sent-notifications-button').first().click();
+    cy.getByDataCy('notification-sent-date').its('length').should('eq', 5);
   });
 });
