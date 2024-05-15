ca24bf7afc1e96cea81b3bf9157a81f4ea270a2e
Aleksander Naskret <aln@strongminds.dk>
2024-05-15 14:08:41 +0200

    cypress test

--- cypress/e2e/data-processings/overview/data-processing.overview.cy.ts
+++ cypress/e2e/data-processings/overview/data-processing.overview.cy.ts
@@ -16,4 +16,15 @@ describe('data-processing', () => {
     cy.contains('Dpa 2');
     cy.contains('Dpa 3');
   });
+
+  it('cant create if name already exists', () => {
+    cy.intercept('/api/v2/data-processing-registrations?nameEquals*', {
+      fixture: './dpr/data-processings-name-exists-search.json',
+    });
+    cy.getByDataCy('create-button').click();
+    cy.inputByCy('create-name').type('DefaultDpa');
+    // The name field waits for 500ms before calling the backend to verify if the name already exists
+    cy.wait(500);
+    cy.getByDataCy('name-error').should('exist');
+  });
 });
