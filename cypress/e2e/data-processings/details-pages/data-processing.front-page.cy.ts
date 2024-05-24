90331cb369a31085bf147bbbb814bf1a27fb4f11
Aleksander Naskret <aln@strongminds.dk>
2024-05-24 14:49:42 +0200

    dpr front page agreement concluded test

--- cypress/e2e/data-processings/details-pages/data-processing.front-page.cy.ts
+++ cypress/e2e/data-processings/details-pages/data-processing.front-page.cy.ts
@@ -8,6 +8,24 @@ describe('it-contracts', () => {
     });
     cy.intercept('/api/v2/data-processing-registrations/permissions*', { fixture: 'shared/create-permissions.json' });
     cy.intercept('/api/v2/data-processing-registrations/*/permissions', { fixture: './shared/permissions.json' });
-    cy.intercept('/api/v2/data-processing-registrations/*', { fixture: './dpr/data-processing.json' });
+    cy.intercept('/api/v2/data-processing-registrations/*', { fixture: './dpr/data-processing-registration.json' });
+    cy.intercept('/api/v2/data-processing-registration-basis-for-transfer-types*', {
+      fixture: './dpr/choice-types/basis-for-transfer-types.json',
+    });
+    cy.intercept('/api/v2/data-processing-registration-data-responsible-types*', {
+      fixture: './dpr/choice-types/data-responsible-types.json',
+    });
+    cy.intercept('/api/v2/-processing-registration-country-types*', {
+      fixture: './dpr/choice-types/country-types.json',
+    });
+    cy.setup(true, 'data-processing');
+  });
+
+  it('Agreement conclusion date is enabled when agreement is concluded', () => {
+    cy.contains('Dpa 1').click();
+
+    cy.getByDataCy('dpr-agreement-concluded-date').find('input').should('be.disabled');
+    cy.dropdownByCy('dpr-agreement-concluded', 'Ja', true);
+    cy.getByDataCy('dpr-agreement-concluded-date').find('input').should('not.be.disabled');
   });
 });
