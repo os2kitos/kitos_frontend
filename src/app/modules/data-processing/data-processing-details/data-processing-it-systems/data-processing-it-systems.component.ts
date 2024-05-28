4aeb7d99d59f9f6d9f1480d402f71bd591db3511
Aleksander Naskret <aln@strongminds.dk>
2024-05-28 10:22:45 +0200

    shell component

---
+++ src/app/modules/data-processing/data-processing-details/data-processing-it-systems/data-processing-it-systems.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-processing-it-systems',
+  templateUrl: './data-processing-it-systems.component.html',
+  styleUrl: './data-processing-it-systems.component.scss'
+})
+export class DataProcessingItSystemsComponent {
+
+}
