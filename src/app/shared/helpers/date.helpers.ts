700c292b5dca72a59b0db2b64a5d444a49d9562a
Aleksander Naskret <aln@strongminds.dk>
2024-05-29 08:13:39 +0200

    cleanup

--- src/app/shared/helpers/date.helpers.ts
+++ src/app/shared/helpers/date.helpers.ts
@@ -7,30 +7,3 @@ export const optionalNewDate = (date?: string): Date | undefined => {
 export const hasValidCache = (cacheTime?: number, time = new Date(), durationMs = oneHourInMs): boolean => {
   return !!cacheTime && cacheTime > time.getTime() - durationMs;
 };
-
-export const toLocalISOString = (date: Date): string => {
-  const tzo = -date.getTimezoneOffset();
-  const dif = tzo >= 0 ? '+' : '-';
-  const pad = function (num: number) {
-    const norm = Math.floor(Math.abs(num));
-    return (norm < 10 ? '0' : '') + norm;
-  };
-
-  return (
-    date.getFullYear() +
-    '-' +
-    pad(date.getMonth() + 1) +
-    '-' +
-    pad(date.getDate()) +
-    'T' +
-    pad(date.getHours()) +
-    ':' +
-    pad(date.getMinutes()) +
-    ':' +
-    pad(date.getSeconds()) +
-    dif +
-    pad(tzo / 60) +
-    ':' +
-    pad(tzo % 60)
-  );
-};
