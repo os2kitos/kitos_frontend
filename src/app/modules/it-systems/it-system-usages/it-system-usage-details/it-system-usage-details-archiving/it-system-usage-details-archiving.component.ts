af64321ab9a8984e3fa64b14d4b28105af2898a2
NikolajNielsen <nan@strongminds.dk>
2024-06-28 09:39:18 +0200

    Cleanup of input validation for archiving frequency

--- src/app/modules/it-systems/it-system-usages/it-system-usage-details/it-system-usage-details-archiving/it-system-usage-details-archiving.component.ts
+++ src/app/modules/it-systems/it-system-usages/it-system-usage-details/it-system-usage-details-archiving/it-system-usage-details-archiving.component.ts
@@ -246,4 +246,8 @@ export class ItSystemUsageDetailsArchivingComponent extends BaseComponent implem
         })
     );
   }
+
+  public journalFrequencyPlaceholder(){
+    return `Indtast et heltal mellem 0 og ${this.journalFrequencyInputUpperLimit}`
+  }
 }
