caaceff79f2c245c7bda832105bbf71b9fc5f57d
Aleksander Naskret <aln@strongminds.dk>
2024-06-28 12:45:52 +0200

    remove kendo locales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,9 +11,9 @@ import { AppComponent } from './app.component';
 import { LayoutModule } from './modules/layout/layout.module';
 import { SharedModule } from './shared/shared.module';
 import { RootStoreModule } from './store/root-store.module';
-
+/*
 import '@progress/kendo-angular-intl/locales/da/all';
-
+ */
 @NgModule({
   declarations: [AppComponent],
   imports: [
