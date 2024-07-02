d45c92060b111b5c1dc5a030cbb66d6423871348
Morten Hoffmann Sørensen <mhs@strongminds.dk>
2024-07-02 12:39:10 +0200

    Rolled back unneeded cypress timeouts

--- cypress.config.ts
+++ cypress.config.ts
@@ -7,11 +7,6 @@ export default defineConfig({
     video: false,
     viewportWidth: 1440,
     viewportHeight: 1000,
-    experimentalRunAllSpecs: true,
-    "defaultCommandTimeout": 120000,
-    "pageLoadTimeout": 120000,
-    "execTimeout": 120000,
-    "taskTimeout": 120000,
-    "responseTimeout": 120000
+    experimentalRunAllSpecs: true
   },
 });
