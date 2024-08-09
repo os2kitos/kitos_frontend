3d4c412441161fa24598b5ff5a18597aeb6b2f57
Aleksander Naskret <aln@strongminds.dk>
2024-08-09 13:51:17 +0200

    contract columns

--- src/app/modules/it-contracts/overview/it-contracts.component.ts
+++ src/app/modules/it-contracts/overview/it-contracts.component.ts
@@ -6,6 +6,7 @@ import { combineLatestWith, first } from 'rxjs';
 import { BaseComponent } from 'src/app/shared/base/base.component';
 import { GridColumn } from 'src/app/shared/models/grid-column.model';
 import { GridState } from 'src/app/shared/models/grid-state.model';
+import { yesNoOptions } from 'src/app/shared/models/yes-no.model';
 import { CONTRACT_COLUMNS_ID } from 'src/app/shared/persistent-state-constants';
 import { StatePersistingService } from 'src/app/shared/services/state-persisting.service';
 import { ITContractActions } from 'src/app/store/it-contract/actions';
@@ -16,6 +17,7 @@ import {
   selectContractGridState,
   selectItContractHasCollectionCreatePermissions,
 } from 'src/app/store/it-contract/selectors';
+import { RegularOptionTypeActions } from 'src/app/store/regular-option-type-store/actions';
 
 @Component({
   templateUrl: 'it-contracts.component.html',
@@ -40,13 +42,120 @@ export class ITContractsComponent extends BaseComponent implements OnInit {
   }
 
   private readonly gridColumns: GridColumn[] = [
-    { field: 'name', title: $localize`IT Kontrakt`, section: 'IT Kontrakter', style: 'primary', hidden: false },
     {
-      field: 'disabled',
-      title: $localize`IT Kontrakt status`,
+      field: 'IsActive',
+      title: $localize`Gyldig/Ikke Gyldig`,
       section: 'IT Kontrakter',
       filter: 'boolean',
       style: 'chip',
+      entityType: 'it-contract',
+      hidden: false,
+    },
+    { field: 'ContractId', title: $localize`Kontrakt ID`, section: 'IT Kontrakter', hidden: false },
+    { field: 'ParentContractName', title: $localize`Overordnet kontrakt`, section: 'IT Kontrakter', hidden: false },
+    { field: 'Name', title: $localize`IT Kontrakt`, section: 'IT Kontrakter', hidden: false },
+    {
+      field: 'Concluded',
+      title: $localize`Gyldig fra`,
+      section: 'IT Kontrakter',
+      filter: 'date',
+      style: 'date',
+      hidden: false,
+    },
+    {
+      field: 'ExpirationDate',
+      title: $localize`Gyldig til`,
+      section: 'IT Kontrakter',
+      filter: 'date',
+      style: 'date',
+      hidden: false,
+    },
+    {
+      field: 'CriticalityName',
+      title: $localize`Kritikalitet`,
+      section: 'IT Kontrakter',
+      extraFilter: 'choice-type',
+      extraData: 'it-contract_criticality-type',
+      hidden: false,
+    },
+    {
+      field: 'ResponsibleOrgUnitName',
+      title: $localize`Ansvarlig org. enhed`,
+      section: 'IT Kontrakter',
+      hidden: false,
+    },
+    { field: 'SupplierName', title: $localize`Leverandør`, section: 'IT Kontrakter', hidden: false },
+    { field: 'ContractSigner', title: $localize`Kontraktunderskriver`, section: 'IT Kontrakter', hidden: false },
+    {
+      field: 'ContractTypeName',
+      title: $localize`Kontrakttype`,
+      section: 'IT Kontrakter',
+      extraFilter: 'choice-type',
+      extraData: 'it-contract_contract-type',
+      hidden: false,
+    },
+    {
+      field: 'ContractTemplateName',
+      title: $localize`Kontraktskabelon`,
+      section: 'IT Kontrakter',
+      extraFilter: 'choice-type',
+      extraData: 'it-contract_contract-template-type',
+      hidden: false,
+    },
+    {
+      field: 'PurchaseFormName',
+      title: $localize`Indkøbsform`,
+      section: 'IT Kontrakter',
+      extraFilter: 'choice-type',
+      extraData: 'it-contract_purchase-form-type',
+      hidden: false,
+    },
+    {
+      field: 'ProcurementStrategyName',
+      title: $localize`Genanskaffelsesstrategi`,
+      section: 'IT Kontrakter',
+      extraFilter: 'choice-type',
+      extraData: 'it-contract_procurement-strategy-type',
+      hidden: false,
+    },
+    {
+      field: 'ProcurementPlan',
+      title: $localize`Genanskaffelsesplan`,
+      section: 'IT Kontrakter',
+      //TODO: should filter by available dates
+      noFilter: true,
+      hidden: false,
+    },
+    {
+      field: 'ProcurementInitiated',
+      title: $localize`Genanskaffelse igangsat`,
+      section: 'IT Kontrakter',
+      extraFilter: 'enum',
+      extraData: yesNoOptions,
+      hidden: false,
+    },
+    {
+      field: 'DataProcessingAgreements',
+      title: $localize`Databehandleraftaler`,
+      style: 'page-link-array',
+      entityType: 'data-processing-registration',
+      section: 'IT Kontrakter',
+      hidden: false,
+    },
+    {
+      field: 'ItSystemUsages',
+      title: $localize`IT Systemer`,
+      style: 'page-link-array',
+      entityType: 'it-system-usage',
+      section: 'IT Kontrakter',
+      hidden: false,
+    },
+    { field: 'SourceEntityUuid', title: $localize`IT Systemer (UUID)`, section: 'IT Kontrakter', hidden: false },
+    {
+      field: 'NumberOfAssociatedSystemRelations',
+      title: $localize`Antal relationer`,
+      section: 'IT Kontrakter',
+      filter: 'numeric',
       hidden: false,
     },
     {
@@ -60,6 +169,7 @@ export class ITContractsComponent extends BaseComponent implements OnInit {
   ];
 
   ngOnInit(): void {
+    RegularOptionTypeActions.getOptions('it-contract_procurement-strategy-type');
     const existingColumns = this.statePersistingService.get<GridColumn[]>(CONTRACT_COLUMNS_ID);
     this.store.dispatch(ITContractActions.getItContractOverviewRoles());
     if (existingColumns) {
--- src/app/shared/models/it-contract/it-contract.model.ts
+++ src/app/shared/models/it-contract/it-contract.model.ts
@@ -1,7 +1,30 @@
+import { mapToYesNoEnum, YesNoOptions } from '../yes-no.model';
+
 export interface ITContract {
   id: string;
-  name: string;
-  disabled: boolean;
+  IsActive: boolean;
+  ContractId: string;
+  ParentContractName: string;
+  //Should be implemented in the backend: ParentContractUuid: string;
+  Name: string;
+  Concluded: Date;
+  ExpirationDate: Date;
+  CriticalityName: string;
+  ResponsibleOrgUnitName: string;
+  SupplierName: string;
+  ContractSigner: string;
+  ContractTypeName: string;
+  ContractTemplateName: string;
+  PurchaseFormName: string;
+  ProcurementStrategyName: string;
+  ProcurementPlan: string;
+  ProcurementInitiated: YesNoOptions | undefined;
+  DataProcessingAgreements: { id: string; value: string }[];
+  ItSystemUsages: { id: string; value: string }[];
+  SourceEntityUuid: string;
+  NumberOfAssociatedSystemRelations: number;
+  ActiveReferenceTitle: string;
+  ActiveReferenceUrl: string;
   lastChangedById: number;
   lastChangedAt: string;
 }
@@ -10,10 +33,39 @@ export interface ITContract {
 export const adaptITContract = (value: any): ITContract | undefined => {
   if (!value.SourceEntityUuid) return;
 
+  const procurementPlan = `${value.ProcurementPlanQuarter} | ${value.ProcurementPlanYear}`;
   return {
     id: value.SourceEntityUuid,
-    name: value.Name,
-    disabled: value.IsActive === 'false',
+    IsActive: value.IsActive,
+    ContractId: value.ContractId,
+    ParentContractName: value.ParentContractName,
+    Name: value.Name,
+    Concluded: value.Concluded,
+    ExpirationDate: value.ExpirationDate,
+    CriticalityName: value.CriticalityName,
+    ResponsibleOrgUnitName: value.ResponsibleOrgUnitName,
+    SupplierName: value.SupplierName,
+    ContractSigner: value.ContractSigner,
+    ContractTypeName: value.ContractTypeName,
+    ContractTemplateName: value.ContractTemplateName,
+    PurchaseFormName: value.PurchaseFormName,
+    ProcurementStrategyName: value.ProcurementStrategyName,
+    ProcurementPlan: procurementPlan,
+    ProcurementInitiated: mapToYesNoEnum(value.ProcurementInitiated),
+    DataProcessingAgreements: value.DataProcessingAgreements.map(
+      (dpa: { uuid: string; DataProcessingRegistrationName: string }) => ({
+        id: 'IMPLEMENT IN BACKEND',
+        value: dpa.DataProcessingRegistrationName,
+      })
+    ),
+    ItSystemUsages: value.ItSystemUsages.map((dpa: { uuid: string; ItSystemName: string }) => ({
+      id: 'IMPLEMENT IN BACKEND',
+      value: dpa.ItSystemName,
+    })),
+    SourceEntityUuid: value.SourceEntityUuid,
+    NumberOfAssociatedSystemRelations: value.NumberOfAssociatedSystemRelations,
+    ActiveReferenceTitle: value.ActiveReferenceTitle,
+    ActiveReferenceUrl: value.ActiveReferenceUrl,
     lastChangedById: value.LastEditedByUserId,
     lastChangedAt: value.LastEditedAtDate,
   };
