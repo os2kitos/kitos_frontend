7030c3940b90d5f62093b0dd5bb3c3255a2b801f
JacobBorchMadsen <jbma@strongminds.dk>
2024-08-09 09:20:28 +0200

    added new files

---
+++ src/app/shared/components/usages/usages.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-usages',
+  standalone: true,
+  imports: [],
+  templateUrl: './usages.component.html',
+  styleUrl: './usages.component.scss'
+})
+export class UsagesComponent {
+
+}
