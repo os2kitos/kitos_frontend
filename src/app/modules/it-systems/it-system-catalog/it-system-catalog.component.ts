0197143ef654c5e8bd1a63862a980a7d8650e99f
NikolajNielsen <nan@strongminds.dk>
2024-08-09 10:59:55 +0200

    Cleanup

--- src/app/modules/it-systems/it-system-catalog/it-system-catalog.component.ts
+++ src/app/modules/it-systems/it-system-catalog/it-system-catalog.component.ts
@@ -3,7 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Actions, ofType } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
 import { CellClickEvent } from '@progress/kendo-angular-grid';
-import { combineLatestWith, first, of } from 'rxjs';
+import { combineLatestWith, first } from 'rxjs';
 import { BaseOverviewComponent } from 'src/app/shared/base/base-overview.component';
 import { accessModifierOptions } from 'src/app/shared/models/access-modifier.model';
 import { GridColumn } from 'src/app/shared/models/grid-column.model';
