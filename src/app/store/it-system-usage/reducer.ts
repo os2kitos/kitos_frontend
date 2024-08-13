438a13ec87e4966d77cebb50c6b3c686c4a111fd
JacobBorchMadsen <jbma@strongminds.dk>
2024-08-13 13:33:18 +0200

    more work but local storage for sort resets on refresh

--- src/app/store/it-system-usage/reducer.ts
+++ src/app/store/it-system-usage/reducer.ts
@@ -161,4 +161,3 @@ export const itSystemUsageFeature = createFeature({
     })
   ),
 });
-
--- src/app/store/meta/local-storage-sync.reducer.ts
+++ src/app/store/meta/local-storage-sync.reducer.ts
@@ -1,9 +1,10 @@
 import { ActionReducer } from '@ngrx/store';
 import { LocalStorageConfig, localStorageSync } from 'ngrx-store-localstorage';
 import { userFeature } from '../user-store/reducer';
+import { itSystemUsageFeature } from '../it-system-usage/reducer';
 
 const localStorageSyncOptions: LocalStorageConfig = {
-  keys: [{ [userFeature.name]: ['organization'] }],
+  keys: [{ [userFeature.name]: ['organization'] }, { [itSystemUsageFeature.name]: ['sort'] }],
   rehydrate: true,
 };
 
