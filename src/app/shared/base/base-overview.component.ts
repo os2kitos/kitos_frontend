25c4e026169f2eaaaeb7eecf209dd149c7184351
NikolajNielsen <nan@strongminds.dk>
2024-08-16 13:32:14 +0200

    Clarify constant name

--- src/app/shared/base/base-overview.component.ts
+++ src/app/shared/base/base-overview.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CellClickEvent } from '@progress/kendo-angular-grid';
+import { DEFAULT_UNCLICKABLE_GRID_COLUMN_STYLES } from '../constants';
 import { GridColumn } from '../models/grid-column.model';
 import { BaseComponent } from './base.component';
-import { DEFAULT_UNCLICKABLE_GRID_COLUMNS } from '../constants';
 
 @Component({
   template: '',
@@ -11,11 +11,14 @@ import { DEFAULT_UNCLICKABLE_GRID_COLUMNS } from '../constants';
 export class BaseOverviewComponent extends BaseComponent {
   protected unclickableColumnsTitles: string[] = [];
 
-  constructor(){
+  constructor() {
     super();
   }
 
-  protected updateUnclickableColumns(currentColumns: GridColumn[], unclickableColumnStyles: string[] = DEFAULT_UNCLICKABLE_GRID_COLUMNS){
+  protected updateUnclickableColumns(
+    currentColumns: GridColumn[],
+    unclickableColumnStyles: string[] = DEFAULT_UNCLICKABLE_GRID_COLUMN_STYLES
+  ) {
     this.unclickableColumnsTitles = [];
     currentColumns.forEach((column) => {
       if (column.style && unclickableColumnStyles.includes(column.style)) {
