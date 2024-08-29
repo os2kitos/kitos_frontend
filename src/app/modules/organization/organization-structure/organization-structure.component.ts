3c5a59d9a9d3a284ef0ef23772416bc647ebdcd2
Aleksander Naskret <aln@strongminds.dk>
2024-08-29 11:04:09 +0200

    organization tabs setup

---
+++ src/app/modules/organization/organization-structure/organization-structure.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-organization-structure',
+  templateUrl: './organization-structure.component.html',
+  styleUrl: './organization-structure.component.scss'
+})
+export class OrganizationStructureComponent {
+
+}
