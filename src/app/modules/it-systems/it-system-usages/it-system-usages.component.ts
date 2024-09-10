47726a152842c6e87b0934607cf0513da21fe9ad
JacobBorchMadsen <jbma@strongminds.dk>
2024-09-10 08:59:22 +0200

    removed comment

--- src/app/modules/it-systems/it-system-usages/it-system-usages.component.ts
+++ src/app/modules/it-systems/it-system-usages/it-system-usages.component.ts
@@ -193,7 +193,7 @@ export class ITSystemUsagesComponent extends BaseOverviewComponent implements On
       persistId: 'orgunit',
     },
     {
-      field: 'RelevantOrganizationUnitNamesAsCsv', //TODO: Need to figure this out
+      field: 'RelevantOrganizationUnitNamesAsCsv',
       title: $localize`Relevante organisationsenheder`,
       section: ORGANISATION_SECTION_NAME,
       width: 350,
