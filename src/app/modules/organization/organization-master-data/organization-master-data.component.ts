7467dc1d8039860ebb547813df2f601c2284e69c
NikolajNielsen <nan@strongminds.dk>
2024-09-23 07:51:06 +0200

    Add readonly form.
    Commented controls correspond to missing fields in our current dto.

--- src/app/modules/organization/organization-master-data/organization-master-data.component.ts
+++ src/app/modules/organization/organization-master-data/organization-master-data.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { selectOrganization } from 'src/app/store/user-store/selectors';
 
 @Component({
   selector: 'app-organization-master-data',
   templateUrl: './organization-master-data.component.html',
-  styleUrl: './organization-master-data.component.scss'
+  styleUrl: './organization-master-data.component.scss',
 })
-export class OrganizationMasterDataComponent {
+export class OrganizationMasterDataComponent implements OnInit {
+  public readonly organization$ = this.store.select(selectOrganization);
+  public readonly masterDataForm = new FormGroup({
+    cvrControl: new FormControl<string | undefined>(undefined),
+    phoneControl: new FormControl<string | undefined>(undefined),
+    emailControl: new FormControl<string | undefined>(undefined),
+    addressControl: new FormControl<string | undefined>(undefined),
+  });
 
+  constructor(private readonly store: Store) {}
+  ngOnInit(): void {
+    this.organization$.subscribe((organization) => {
+      this.masterDataForm.patchValue({
+        cvrControl: organization?.cvr,
+        //phoneControl: organization.phone,
+        //emailControl: organization.email
+        //addressControl: organization.address
+      });
+    });
+  }
 }
