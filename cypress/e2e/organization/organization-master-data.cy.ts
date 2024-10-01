61a7ef1ed4f7cb4670d386a859886aeb90f5c9a5
NikolajNielsen <nan@strongminds.dk>
2024-10-01 08:19:51 +0200

    Test can patch data responsible role (unstable)

--- cypress/e2e/organization/organization-master-data.cy.ts
+++ cypress/e2e/organization/organization-master-data.cy.ts
@@ -12,7 +12,7 @@ describe('organization-master-data', () => {
   });
 
   it('Can edit organization master data', () => {
-    const newCvr = 'newCvr';
+    const newCvr = 'newOrgCvr';
     cy.intercept('/api/v2/internal/organizations/*/masterData').as('patch');
 
     cy.getByDataCy('master-data-cvr-input').type('{selectall}{backspace}').type(newCvr);
@@ -20,6 +20,18 @@ describe('organization-master-data', () => {
 
     cy.wait('@patch').then((interception) => {
       expect(interception.request.body).to.deep.equal({ cvr: newCvr });
+    });
+  });
+
+  it('Can edit data responsible role master data', () => {
+    const newCvr = 'newDrCvr';
+    cy.intercept('/api/v2/internal/organizations/*/masterData/roles').as('patch');
+
+    cy.getByDataCy('data-responsible-cvr-input').type('{selectall}{backspace}').type(newCvr);
+    cy.getByDataCy('master-data-headline').click();
+
+    cy.wait('@patch').then((interception) => {
+      expect(interception.request.body.dataResponsible).to.have.property('cvr', newCvr);
     });
   });
 });
