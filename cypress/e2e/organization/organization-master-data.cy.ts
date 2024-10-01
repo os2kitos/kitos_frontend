6313f909205d59f7b32f74e47b1c9dcb4fa999a7
NikolajNielsen <nan@strongminds.dk>
2024-10-01 08:40:34 +0200

    Selecting contact email from dropdown disables other fields

--- cypress/e2e/organization/organization-master-data.cy.ts
+++ cypress/e2e/organization/organization-master-data.cy.ts
@@ -8,6 +8,7 @@ describe('organization-master-data', () => {
     cy.intercept('/api/v2/internal/organizations/*/masterData/roles', {
       fixture: './organizations/organization-master-data-roles.json',
     });
+    cy.intercept('/api/v2/organizations/*/users', { fixture: './organizations/organization-users.json' });
     cy.setup(true, 'organization/master-data');
   });
 
@@ -33,5 +34,18 @@ describe('organization-master-data', () => {
     cy.wait('@patch').then((interception) => {
       expect(interception.request.body.dataResponsible).to.have.property('cvr', newCvr);
     });
+  });
+
+  it('Selecting contact person email from dropdown disables other contact person fields', () => {
+    cy.intercept('api/v2/internal/organization/*/users/permissions', {
+      fixture: './organizations/users/permissions.json',
+    });
+    cy.intercept('/api/v2/organizations/*/users', { fixture: './organizations/organization-users.json' });
+    cy.intercept('/api/v2/internal/organizations/*/masterData/roles').as('patch');
+
+    cy.dropdownByCy('contact-person-email-dropdown', 'local-global-admin-user@kitos.dk', true);
+    cy.getByDataCy('contact-person-name-control').get('input').should('be.disabled');
+    cy.getByDataCy('contact-person-last-name-control').get('input').should('be.disabled');
+    cy.getByDataCy('contact-person-phone-control').get('input').should('be.disabled');
   });
 });
