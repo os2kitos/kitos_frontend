a53dea5b88f9cb6280a344db5cf3f250974f352b
NikolajNielsen <nan@strongminds.dk>
2024-10-06 10:50:16 +0200

    Add local admin status to user model

--- src/app/shared/models/user.model.ts
+++ src/app/shared/models/user.model.ts
@@ -5,15 +5,20 @@ export interface User {
   email: string;
   fullName: string;
   isGlobalAdmin: boolean;
+  isLocalAdmin: boolean;
 }
 
+const localAdminEnumValue = 1;
+
 export const adaptUser = (apiUser?: APIUserDTO): User | undefined => {
-  if (apiUser?.id === undefined || apiUser?.email === undefined) return;
+if (apiUser?.id === undefined || apiUser?.email === undefined) return;
 
   return {
     id: apiUser.id,
     email: apiUser.email,
     fullName: apiUser?.fullName ?? '',
     isGlobalAdmin: apiUser?.isGlobalAdmin ?? false,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    isLocalAdmin: apiUser.organizationRights?.map(right => (right as any).role).includes(localAdminEnumValue) ?? false,
   };
 };
