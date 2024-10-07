aca24ca92e59f570a223251e3448fe32a87f7000
NikolajNielsen <nan@strongminds.dk>
2024-10-07 13:23:32 +0200

    Add info section layout and name control

--- src/app/modules/local-admin/local-admin-information/local-admin-information.component.ts
+++ src/app/modules/local-admin/local-admin-information/local-admin-information.component.ts
@@ -1,16 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { Store } from '@ngrx/store';
-import { selectOrganizationName } from 'src/app/store/user-store/selectors';
+import { BaseComponent } from 'src/app/shared/base/base.component';
+import { OrganizationMasterDataActions } from 'src/app/store/organization/organization-master-data/actions';
+import { selectOrganizationHasModifyCvrPermission } from 'src/app/store/organization/organization-master-data/selectors';
+import { selectOrganizationCvr, selectOrganizationName, selectOrganizationType } from 'src/app/store/user-store/selectors';
 
 @Component({
   selector: 'app-local-admin-information',
   templateUrl: './local-admin-information.component.html',
-  styleUrl: './local-admin-information.component.scss'
+  styleUrl: './local-admin-information.component.scss',
 })
-export class LocalAdminInformationComponent {
+export class LocalAdminInformationComponent extends BaseComponent implements OnInit {
   public readonly organizationName$ = this.store.select(selectOrganizationName);
+  public readonly hasModifyCvrPermission$ = this.store.select(selectOrganizationHasModifyCvrPermission);
+  public readonly organizationCvr$ = this.store.select(selectOrganizationCvr);
+  public readonly organizationType$ = this.store.select(selectOrganizationType);
 
 
-  constructor(private readonly store: Store){}
+  public readonly form = new FormGroup({
+    nameControl: new FormControl<string | undefined>(undefined),
+    cvrControl: new FormControl<number | undefined>(undefined),
+    typeControl: new FormControl<string | undefined>(undefined),
+  });
 
+  constructor(private readonly store: Store) {
+    super();
+  }
+
+  ngOnInit(): void {
+    this.store.dispatch(OrganizationMasterDataActions.getOrganizationPermissions());
+
+    this.subscriptions.add(
+      this.organizationName$.subscribe((organizationName) => {
+        console.log(organizationName + ' is org')
+        this.form.patchValue({
+          nameControl: organizationName,
+        });
+      })
+    );
+
+    this.hasModifyCvrPermission$.subscribe((hasModifyCvrPermission) => {
+      if (!hasModifyCvrPermission) this.form.controls.cvrControl.disable();
+    })
+  }
 }
