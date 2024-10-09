90d4cfb86f4c80d12b3dda9db2a338d54dedf165
NikolajNielsen <nan@strongminds.dk>
2024-10-09 14:17:10 +0200

    Improve test to also confirm patch call

--- cypress/e2e/local-admin/local-admin-information.cy.ts
+++ cypress/e2e/local-admin/local-admin-information.cy.ts
@@ -8,18 +8,25 @@ describe('local-admin', () => {
 
   it('can edit just organization name if local admin', () => {
     const newName = 'someOrg';
-    cy.intercept('/api/v2/internal/organizations/*/patch').as('patch');
-    cy.intercept(
-      'http://localhost:4200/api/v2/internal/organizations/3dc52c64-3706-40f4-bf58-45035bb376da/permissions',
-      {
-        fixture: './organizations/organization-permissions-local-admin.json',
-      }
-    );
+    cy.intercept('/api/v2/internal/organizations/*/permissions', {
+      fixture: './organizations/organization-permissions-local-admin.json',
+    });
+    cy.intercept('PATCH', '/api/v2/internal/organizations/*/patch', {
+      statusCode: 200,
+      body: { }
+    }).as('patch');
+
 
     cy.hoverByDataCy('profile-menu');
     cy.getByDataCy('local-admin-menu-item').should('exist').click();
 
     cy.confirmTextboxStateByDataCy('name-input', true);
     cy.confirmTextboxStateByDataCy('cvr-input', false);
+    cy.replaceTextByDataCy('name-input', newName);
+    cy.getByDataCy('info-title').click();
+
+    cy.wait('@patch', { timeout: 10000 }).then((interception) => {
+      expect(interception.request.body.name).to.equal(newName);
+    });
   });
 });
