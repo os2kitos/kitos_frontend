52a8b2c332e688ea05907e88ba4820da07fe8e8a
NikolajNielsen <nan@strongminds.dk>
2024-10-09 14:19:13 +0200

    Test global admin can also edit cvr

--- cypress/e2e/local-admin/local-admin-information.cy.ts
+++ cypress/e2e/local-admin/local-admin-information.cy.ts
@@ -29,4 +29,27 @@ describe('local-admin', () => {
       expect(interception.request.body.name).to.equal(newName);
     });
   });
+
+  it('can edit cvr if global admin', () => {
+    const newCvr = '1234';
+    cy.intercept('/api/v2/internal/organizations/*/permissions', {
+      fixture: './organizations/organization-permissions-global-admin.json',
+    });
+    cy.intercept('PATCH', '/api/v2/internal/organizations/*/patch', {
+      statusCode: 200,
+      body: { }
+    }).as('patch');
+
+
+    cy.hoverByDataCy('profile-menu');
+    cy.getByDataCy('local-admin-menu-item').should('exist').click();
+
+    cy.confirmTextboxStateByDataCy('cvr-input', true);
+    cy.replaceTextByDataCy('cvr-input', newCvr);
+    cy.getByDataCy('info-title').click();
+
+    cy.wait('@patch', { timeout: 10000 }).then((interception) => {
+      expect(interception.request.body.cvr).to.equal(newCvr);
+    });
+  });
 });
