4edfdf5a864da1e226fecf1458da192131f14ed8
Aleksander Naskret <aln@strongminds.dk>
2024-10-15 06:49:36 +0200

    base component

---
+++ src/app/modules/local-admin/local-admin-import/local-admin-import.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-local-admin-import',
+  standalone: true,
+  imports: [],
+  templateUrl: './local-admin-import.component.html',
+  styleUrl: './local-admin-import.component.scss'
+})
+export class LocalAdminImportComponent {
+
+}
