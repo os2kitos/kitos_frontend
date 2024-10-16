a2c33047458e02d1fdcd0b8d57836c7be22554e5
NikolajNielsen <nan@strongminds.dk>
2024-10-16 11:47:43 +0200

    Setup for handling checkbox changes in accordion headline

--- src/app/shared/components/accordion/accordion.component.ts
+++ src/app/shared/components/accordion/accordion.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-accordion',
@@ -10,4 +10,11 @@ export class AccordionComponent {
   @Input() isExpandedByDefault = false;
   @Input() disabled = false;
   @Input() checkboxInTitle = false;
+  @Input() checkboxDisabled = false;
+  @Input() checkboxValue = false;
+  @Output() checkboxToggled = new EventEmitter<boolean>();
+
+  public onCheckboxToggled($event: boolean | undefined){
+    this.checkboxToggled.emit($event);
+  }
 }
