28bdfa825ee32c689c7d7f1521a1ac9b4b704822
NikolajNielsen <nan@strongminds.dk>
2024-10-16 14:32:42 +0200

    Add store access to component

--- src/app/modules/local-admin/local-admin-it-system-usages/local-admin-it-system-usages.component.ts
+++ src/app/modules/local-admin/local-admin-it-system-usages/local-admin-it-system-usages.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { Store } from '@ngrx/store';
 import { SegmentButtonOption } from 'src/app/shared/components/segment/segment.component';
+import { ItSystemUsages } from 'src/app/shared/ui-module-customization-keys.constants';
+import { OrganizationUiModuleCustomizationActions } from 'src/app/store/organization/organization-ui-customization/actions';
+import { selectITSystemUsagesUIModuleCustomization } from 'src/app/store/organization/organization-ui-customization/selectors';
 
 enum LocalAdminSystemUsagesSegmentOptions {
   UiCustomization = 'UiCustomization',
@@ -11,13 +16,24 @@ enum LocalAdminSystemUsagesSegmentOptions {
   templateUrl: './local-admin-it-system-usages.component.html',
   styleUrl: './local-admin-it-system-usages.component.scss',
 })
-export class LocalAdminItSystemUsagesComponent {
+export class LocalAdminItSystemUsagesComponent implements OnInit {
   public readonly LocalAdminSystemUsagesSegmentOptions = LocalAdminSystemUsagesSegmentOptions;
-
   public selectedSegment: LocalAdminSystemUsagesSegmentOptions = LocalAdminSystemUsagesSegmentOptions.UiCustomization;
-
   public readonly segmentOptions: SegmentButtonOption<LocalAdminSystemUsagesSegmentOptions>[] = [
     { text: $localize`Lokal tilpasning af brugerfladen`, value: LocalAdminSystemUsagesSegmentOptions.UiCustomization },
     { text: $localize`Udfaldsrum`, value: LocalAdminSystemUsagesSegmentOptions.OptionTypes },
   ];
+  public readonly itSystemUsageUIModuleCustomization$ = this.store.select(selectITSystemUsagesUIModuleCustomization);
+
+  public readonly itSystemUsageUIModuleCustomizationForm = new FormGroup({});
+
+  constructor(private readonly store: Store) {}
+
+  ngOnInit(): void {
+    this.store.dispatch(
+      OrganizationUiModuleCustomizationActions.getUIModuleCustomization({ moduleName: ItSystemUsages })
+    );
+
+    this.itSystemUsageUIModuleCustomization$.subscribe((u) => console.log(JSON.stringify(u)));
+  }
 }
