4eedbec0369d21e7450cbb67d32b2f38295ca8d7
JacobBorchMadsen <jbma@strongminds.dk>
2024-10-17 15:10:00 +0200

    basic segment setup for local admin it system

--- src/app/modules/local-admin/local-admin-it-system/local-admin-it-system.component.ts
+++ src/app/modules/local-admin/local-admin-it-system/local-admin-it-system.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { SegmentButtonOption } from 'src/app/shared/components/segment/segment.component';
+
+enum LocalAdminItSystemSegmentOption {
+  RegularOptionTypes = 'RegularOptionTypes',
+  RoleOptionTypes = 'RoleOptionTypes',
+}
 
 @Component({
   selector: 'app-local-admin-it-system',
   templateUrl: './local-admin-it-system.component.html',
-  styleUrl: './local-admin-it-system.component.scss'
+  styleUrl: './local-admin-it-system.component.scss',
 })
 export class LocalAdminItSystemComponent {
+  public readonly LocalAdminItSystemSegmentOption = LocalAdminItSystemSegmentOption;
 
+  public selectedSegment: LocalAdminItSystemSegmentOption = LocalAdminItSystemSegmentOption.RegularOptionTypes;
+
+  public readonly segmentOptions: SegmentButtonOption<LocalAdminItSystemSegmentOption>[] = [
+    { text: $localize`It system et eller andet godt avn`, value: LocalAdminItSystemSegmentOption.RegularOptionTypes },
+    { text: $localize`It system roller`, value: LocalAdminItSystemSegmentOption.RoleOptionTypes },
+  ];
 }
