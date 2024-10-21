6d01b57b883fcb68bca76a148fca2b26c148d25b
NikolajNielsen <nan@strongminds.dk>
2024-10-21 20:18:52 +0200

    Add field level checks for usage

--- src/app/modules/it-systems/it-system-usages/it-system-usage-details/it-system-usage-details-frontpage-information/it-system-usage-details-frontpage-information.component.ts
+++ src/app/modules/it-systems/it-system-usages/it-system-usage-details/it-system-usage-details-frontpage-information/it-system-usage-details-frontpage-information.component.ts
@@ -30,6 +30,7 @@ import {
   selectItSystemUsageGeneral,
   selectItSystemUsageValid,
 } from 'src/app/store/it-system-usage/selectors';
+import { selectITSystemUsageUIModuleConfigEnabledFieldFrontPageLifeCycleStatus, selectITSystemUsageUIModuleConfigEnabledFieldFrontPageUsagePeriod } from 'src/app/store/organization/ui-module-customization/selectors';
 import { RegularOptionTypeActions } from 'src/app/store/regular-option-type-store/actions';
 import { selectRegularOptionTypes } from 'src/app/store/regular-option-type-store/selectors';
 @Component({
@@ -49,6 +50,9 @@ export class ITSystemUsageDetailsFrontpageInformationComponent extends BaseCompo
     },
     { updateOn: 'blur' }
   );
+
+  public readonly lifeCycleStatusEnabled$ = this.store.select(selectITSystemUsageUIModuleConfigEnabledFieldFrontPageLifeCycleStatus);
+  public readonly usagePeriodEnabled$ = this.store.select(selectITSystemUsageUIModuleConfigEnabledFieldFrontPageUsagePeriod);
 
   public readonly itSystemApplicationForm = new FormGroup(
     {
