c2fdb427dd5cac28e813145cd25aac5dbd0bebc9
JacobBorchMadsen <jbma@strongminds.dk>
2024-10-22 13:49:36 +0200

    refactor local option type effect to better handle concurrent operations (#232)

--- src/app/store/local-option-types/effects.ts
+++ src/app/store/local-option-types/effects.ts
@@ -2,10 +2,9 @@ import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
 import { LocalOptionTypeActions } from './actions';
-import { concatLatestFrom } from '@ngrx/operators';
 import { selectOrganizationUuid } from '../user-store/selectors';
 import { filterNullish } from 'src/app/shared/pipes/filter-nullish';
-import { catchError, map, of, switchMap } from 'rxjs';
+import { catchError, concatMap, first, groupBy, map, mergeMap, of, switchMap } from 'rxjs';
 import { LocalOptionTypeService } from 'src/app/shared/services/local-option-type.service';
 
 @Injectable()
@@ -18,27 +17,37 @@ export class LocalOptionTypeEffects {
 
   patchOptionType$ = createEffect(() => {
     return this.actions$.pipe(
-      ofType(LocalOptionTypeActions.uppdateOptionType),
-      concatLatestFrom(() => this.store.select(selectOrganizationUuid).pipe(filterNullish())),
-      switchMap(([{ optionType, optionUuid, request }, organizationUuid]) => {
-        return this.localOptionTypeService.patchLocalOption(optionType, organizationUuid, optionUuid, request).pipe(
-          map(() => LocalOptionTypeActions.updateOptionTypeSuccess(optionType)),
-          catchError(() => of(LocalOptionTypeActions.updateOptionTypeError()))
-        );
-      })
-    );
-  });
-
-  patchActiveStatus$ = createEffect(() => {
-    return this.actions$.pipe(
-      ofType(LocalOptionTypeActions.updateOptionTypeActiveStatus),
-      concatLatestFrom(() => this.store.select(selectOrganizationUuid).pipe(filterNullish())),
-      switchMap(([{ optionType, optionUuid, isActive }, organizationUuid]) => {
-        return this.localOptionTypeService.patchIsActive(optionType, organizationUuid, optionUuid, isActive).pipe(
-          map(() => LocalOptionTypeActions.updateOptionTypeSuccess(optionType)),
-          catchError(() => of(LocalOptionTypeActions.updateOptionTypeError()))
-        );
-      })
+      ofType(LocalOptionTypeActions.uppdateOptionType, LocalOptionTypeActions.updateOptionTypeActiveStatus),
+      groupBy((action) => action.optionUuid),
+      mergeMap((group$) =>
+        group$.pipe(
+          concatMap((action) => {
+            return this.store.select(selectOrganizationUuid).pipe(
+              filterNullish(),
+              first(),
+              switchMap((organizationUuid) => {
+                if (action.type === LocalOptionTypeActions.uppdateOptionType.type) {
+                  return this.localOptionTypeService.patchLocalOption(
+                    action.optionType,
+                    organizationUuid,
+                    action.optionUuid,
+                    action.request
+                  );
+                } else {
+                  return this.localOptionTypeService.patchIsActive(
+                    action.optionType,
+                    organizationUuid,
+                    action.optionUuid,
+                    action.isActive
+                  );
+                }
+              }),
+              map(() => LocalOptionTypeActions.updateOptionTypeSuccess(action.optionType)),
+              catchError(() => of(LocalOptionTypeActions.updateOptionTypeError()))
+            );
+          })
+        )
+      )
     );
   });
 }
