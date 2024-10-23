fc41fb79420cdbf27a21dbb683f1740299811fa2
NikolajNielsen <nan@strongminds.dk>
2024-10-23 14:49:36 +0200

    Fix routes

--- src/app/modules/local-admin/local-admin.routes.ts
+++ src/app/modules/local-admin/local-admin.routes.ts
@@ -5,7 +5,6 @@ import { LocalAdminImportComponent } from './local-admin-import/local-admin-impo
 import { LocalAdminInformationComponent } from './local-admin-information/local-admin-information.component';
 import { LocalAdminItSystemUsageComponent } from './local-admin-it-system-usage/local-admin-it-system-usage.component';
 import { LocalAdminOrganizationComponent } from './local-admin-organization/local-admin-organization.component';
-import { LocalAdminItSystemComponent } from './local-admin-it-system/local-admin-it-system.component';
 import { LocalAdminItContractComponent } from './local-admin-it-contract/local-admin-it-contract.component';
 import { LocalAdminDprComponent } from './local-admin-dpr/local-admin-dpr.component';
 import { LocalAdminComponent } from './local-admin.component';
