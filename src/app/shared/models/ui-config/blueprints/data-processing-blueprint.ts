b8816939b33102351c296c8e2be55726c4da533b
JacobBorchMadsen <jbma@strongminds.dk>
2024-10-23 15:15:30 +0200

    add blueprint for data processing ui

---
+++ src/app/shared/models/ui-config/blueprints/data-processing-blueprint.ts
@@ -0,0 +1,50 @@
+import { UIModuleConfigKey } from 'src/app/shared/enums/ui-module-config-key';
+import { uiConfigHelpTexts } from '../ui-config-helptexts';
+
+export const DataProcessingUiCustomizationBluePrint = {
+  module: UIModuleConfigKey.DataProcessingRegistrations,
+  isObligatory: false,
+  helpText: uiConfigHelpTexts.generalUiCustomizationHelpText,
+  text: 'Databehandling',
+  children: {
+    frontPage: {
+      text: 'Forside',
+      isObligatory: true,
+      helpText: uiConfigHelpTexts.cannotChangeTab,
+    },
+    itSystems: {
+      text: 'IT Systemer',
+      helpText: uiConfigHelpTexts.cannotChangeTabOnlyThroughModuleConfig,
+      isObligatory: true,
+    },
+    itContracts: {
+      text: 'IT Kontrakter',
+      helpText: uiConfigHelpTexts.cannotChangeTabOnlyThroughModuleConfig,
+      isObligatory: true,
+      children: {
+        mainContract: {
+          text: 'Hvilken kontrakt skal angive om databehandlingen er aktiv',
+        },
+      },
+    },
+    oversight: {
+      text: 'Tilsyn',
+      helpText: uiConfigHelpTexts.cannotChangeTabOnlyThroughModuleConfig,
+      isObligatory: true,
+      children: {
+        scheduledInspectionDate: {
+          text: 'Kommende planlagt tilsyn',
+        },
+      },
+    },
+    roles: {
+      text: 'Databehandlingsroller',
+    },
+    notifications: {
+      text: 'Advis',
+    },
+    references: {
+      text: 'Referencer',
+    },
+  },
+};
