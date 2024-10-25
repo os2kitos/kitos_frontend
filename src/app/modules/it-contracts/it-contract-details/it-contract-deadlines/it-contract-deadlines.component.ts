c8b40c0c0ce78e1ecedf7698aaf52c42f286114d
JacobBorchMadsen <jbma@strongminds.dk>
2024-10-25 09:57:42 +0200

    implement field customization for it contract agreements

--- src/app/modules/it-contracts/it-contract-details/it-contract-deadlines/it-contract-deadlines.component.ts
+++ src/app/modules/it-contracts/it-contract-details/it-contract-deadlines/it-contract-deadlines.component.ts
@@ -21,6 +21,7 @@ import { filterNullish } from 'src/app/shared/pipes/filter-nullish';
 import { NotificationService } from 'src/app/shared/services/notification.service';
 import { ITContractActions } from 'src/app/store/it-contract/actions';
 import { selectContract, selectItContractHasModifyPermissions } from 'src/app/store/it-contract/selectors';
+import { selectDataProcessingUIModuleConfigEnabledFieldAgreementDeadlines, selectDataProcessingUIModuleConfigEnabledFieldTermination } from 'src/app/store/organization/ui-module-customization/selectors';
 import { RegularOptionTypeActions } from 'src/app/store/regular-option-type-store/actions';
 import { selectRegularOptionTypes } from 'src/app/store/regular-option-type-store/selectors';
 
@@ -62,6 +63,9 @@ export class ItContractDeadlinesComponent extends BaseComponent implements OnIni
     noticePeriodExtendsCurrent: new FormControl<YearSegmentChoice | undefined>({ value: undefined, disabled: true }),
     noticeByEndOf: new FormControl<YearSegmentChoice | undefined>({ value: undefined, disabled: true }),
   });
+
+  public readonly agreementDeadlinesEnabled$ = this.store.select(selectDataProcessingUIModuleConfigEnabledFieldAgreementDeadlines);
+  public readonly terminationEnabled$ = this.store.select(selectDataProcessingUIModuleConfigEnabledFieldTermination);
 
   constructor(private readonly store: Store, private readonly notificationService: NotificationService) {
     super();
