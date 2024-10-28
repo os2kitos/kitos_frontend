ffc9ca3c0ebab6256d9bbe14aef233635d976351
JacobBorchMadsen <jbma@strongminds.dk>
2024-10-28 11:53:35 +0100

    move location of ui-config-tab-section to ui config component folder

--- src/app/modules/local-admin/local-admin.module.ts
+++ src/app/modules/local-admin/local-admin.module.ts
@@ -8,7 +8,7 @@ import { LocalAdminOrganizationComponent } from './local-admin-organization/loca
 import { OrganizationsGridComponent } from './local-admin-organization/organizations-grid/organizations-grid.component';
 import { LocalAdminComponent } from './local-admin.component';
 import { LocalAdminRouterModule } from './local-admin.routes';
-import { UiConfigTabSectionComponent } from './ui-config-tab-section/ui-config-tab-section.component';
+import { UiConfigTabSectionComponent } from './ui-config/ui-config-tab-section/ui-config-tab-section.component';
 import { LocalAdminItContractComponent } from './local-admin-it-contract/local-admin-it-contract.component';
 import { LocalAdminDprComponent } from './local-admin-dpr/local-admin-dpr.component';
 import { FkOrgDeleteDialogComponent } from './local-admin-import/local-admin-import-organization/fk-org-delete-dialog/fk-org-delete-dialog.component';
