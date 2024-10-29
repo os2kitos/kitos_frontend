3b455a533c4132de8173e1e28408b32ff6e7f928
NikolajNielsen <nan@strongminds.dk>
2024-10-29 09:25:08 +0100

    Add local swagger option

---
+++ src/use-local-proxy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+replace_proxy() {
+  local search=$1
+  local replace=$2
+  local file_path=$3
+  vim -c "%s/$search/$replace/g" -c "wq" "$(git rev-parse --show-toplevel)/$file_path"
+}
+
+cleanup_start() {
+  replace_proxy "localhost:44300" "kitos-dev\\.strongminds\\.dk" "src/proxy.conf.json"
+}
+
+cleanup_swagger() {
+  replace_proxy "localhost:44300" "kitos-dev\\.strongminds\\.dk" "openapitools.json"
+}
+
+
+if [ "$1" = "start" ]; then
+  trap cleanup_start EXIT
+
+  replace_proxy "kitos-dev\\.strongminds\\.dk" "localhost:44300" "src/proxy.conf.json"
+  yarn start
+fi
+
+ if [ "$1" = "swagger" ]; then
+   trap cleanup_swagger EXIT
+
+   replace_proxy "kitos-dev\\.strongminds\\.dk" "localhost:44300" "openapitools.json"
+   yarn swagger
+ fi
