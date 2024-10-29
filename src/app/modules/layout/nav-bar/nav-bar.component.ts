4016e1f3113d2c4a7a54d9163a77abf4ed32a3bd
NikolajNielsen <nan@strongminds.dk>
2024-10-29 07:04:04 +0100

    Fix remove incorrect error notification on logout

--- src/app/modules/layout/nav-bar/nav-bar.component.ts
+++ src/app/modules/layout/nav-bar/nav-bar.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { Router } from '@angular/router';
+import { NavigationEnd, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { tap } from 'rxjs';
+import { combineLatest, filter, tap } from 'rxjs';
 import { BaseComponent } from 'src/app/shared/base/base.component';
 import { OrganizationActions } from 'src/app/store/organization/actions';
 import { selectUIRootConfig } from 'src/app/store/organization/selectors';
 import { UserActions } from 'src/app/store/user-store/actions';
-import { selectHasMultipleOrganizations, selectOrganizationName, selectUser } from 'src/app/store/user-store/selectors';
+import { selectHasMultipleOrganizations, selectHasTriedAuthenticating, selectIsAuthenticating, selectOrganizationName, selectUser } from 'src/app/store/user-store/selectors';
 import { AppPath } from '../../../shared/enums/app-path';
 import { ChooseOrganizationComponent } from '../choose-organization/choose-organization.component';
 
@@ -20,6 +20,7 @@ export class NavBarComponent extends BaseComponent implements OnInit {
   public readonly AppPath = AppPath;
 
   public readonly user$ = this.store.select(selectUser);
+  public readonly userHasTriedAuthenticating$ = this.store.select(selectHasTriedAuthenticating);
   public readonly organizationName$ = this.store.select(selectOrganizationName);
   public readonly hasMultipleOrganizations$ = this.store.select(selectHasMultipleOrganizations);
   public readonly uiRootConfig$ = this.store.select(selectUIRootConfig);
@@ -30,8 +31,10 @@ export class NavBarComponent extends BaseComponent implements OnInit {
 
   ngOnInit(): void {
     this.subscriptions.add(
-      this.router.events.pipe(tap(() => this.store.dispatch(OrganizationActions.getUIRootConfig()))).subscribe()
-    );
+      combineLatest([this.userHasTriedAuthenticating$, this.router.events]).pipe(
+        filter(([userHasTriedAuthenticating, event]) => userHasTriedAuthenticating && event instanceof NavigationEnd),
+        tap(() => this.store.dispatch(OrganizationActions.getUIRootConfig()))
+      ).subscribe());
   }
 
   public showOrganizationDialog() {
