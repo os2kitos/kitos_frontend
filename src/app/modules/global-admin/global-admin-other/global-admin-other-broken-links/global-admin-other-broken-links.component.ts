fc37dbf3a99f3873c820daa05961bab8c71b5709
Aleksander Naskret <aln@strongminds.dk>
2024-11-13 13:14:58 +0100

    base component

---
+++ src/app/modules/global-admin/global-admin-other/global-admin-other-broken-links/global-admin-other-broken-links.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-global-admin-other-broken-links',
+  templateUrl: './global-admin-other-broken-links.component.html',
+  styleUrl: './global-admin-other-broken-links.component.scss'
+})
+export class GlobalAdminOtherBrokenLinksComponent {
+
+}
