a432157ea4c62ec4de209b4100e4cfc6fc5dcd1c
Aleksander Naskret <aln@strongminds.dk>
2024-11-14 08:26:42 +0100

    broken links

--- src/app/modules/global-admin/global-admin-other/global-admin-other-broken-links/global-admin-other-broken-links.component-store.ts
+++ src/app/modules/global-admin/global-admin-other/global-admin-other-broken-links/global-admin-other-broken-links.component-store.ts
@@ -2,17 +2,20 @@ import { Inject, Injectable } from '@angular/core';
 import { ComponentStore } from '@ngrx/component-store';
 import { tapResponse } from '@ngrx/operators';
 import * as saveAs from 'file-saver';
-import { pipe, tap } from 'rxjs';
-import { APIV2BrokenExternalReferencesReportInternalINTERNALService } from 'src/app/api/v2';
+import { mergeMap, tap } from 'rxjs';
+import {
+  APIBrokenExternalReferencesReportStatusResponseDTO,
+  APIV2BrokenExternalReferencesReportInternalINTERNALService,
+} from 'src/app/api/v2';
 import { NotificationService } from 'src/app/shared/services/notification.service';
 
 interface State {
   isLoading: boolean;
-  status: object;
+  status?: APIBrokenExternalReferencesReportStatusResponseDTO;
 }
 
 @Injectable()
-export class GlobalAdminOtherUserShutdownComponentStore extends ComponentStore<State> {
+export class GlobalAdminOtherBrokenLinksComponentStore extends ComponentStore<State> {
   public readonly isLoading$ = this.select((state) => state.isLoading);
   public readonly status$ = this.select((state) => state.status);
 
@@ -21,51 +24,64 @@ export class GlobalAdminOtherUserShutdownComponentStore extends ComponentStore<S
     private statusService: APIV2BrokenExternalReferencesReportInternalINTERNALService,
     private notificationService: NotificationService
   ) {
-    super({ isLoading: false, status: [] });
+    super({ isLoading: false });
   }
 
   private readonly setLoading = this.updater((state, isLoading: boolean): State => ({ ...state, isLoading }));
   private readonly setStatus = this.updater(
-    (state, status: object | undefined): State => ({
+    (state, status: APIBrokenExternalReferencesReportStatusResponseDTO | undefined): State => ({
       ...state,
-      status: status ?? [],
+      status: status,
     })
   );
 
-  public getStatus = this.effect(() => {
-    this.setLoading(true);
-    return this.statusService.getSingleBrokenExternalReferencesReportInternalV2GetStatus().pipe(
-      pipe(
-        tap(() => this.setLoading(true)),
-        tapResponse(
-          (status) => this.setStatus(status),
-          (e) => console.error(e),
-          () => this.setLoading(false)
-        )
-      )
-    );
-  });
-
-  public getReport = this.effect(() => {
-    return this.statusService
-      .getSingleBrokenExternalReferencesReportInternalV2GetCurrentCsvReport('response', undefined, {
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        httpHeaderAccept: 'blob' as any,
-        context: undefined,
+  public getStatus = this.effect<void>((trigger$) =>
+    trigger$.pipe(
+      tap(() => this.setLoading(true)),
+      mergeMap(() => {
+        return this.statusService.getSingleBrokenExternalReferencesReportInternalV2GetStatus().pipe(
+          tapResponse(
+            (status) => this.setStatus(status as APIBrokenExternalReferencesReportStatusResponseDTO),
+            (e) => console.error(e),
+            () => this.setLoading(false)
+          )
+        );
       })
-      .pipe(
-        tapResponse(
-          (response) => {
-            console.log(response);
+    )
+  );
+
+  public getReport = this.effect<void>((trigger$) =>
+    trigger$.pipe(
+      tap(() => this.setLoading(true)),
+      mergeMap(() => {
+        return this.statusService
+          .getSingleBrokenExternalReferencesReportInternalV2GetCurrentCsvReport('response', undefined, {
             // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            saveAs(response as any);
-            this.notificationService.showDefault($localize`Brugeren blev slettet`);
-          },
-          (e) => {
-            console.error(e);
-            this.notificationService.showError($localize`Kunne ikke slette brugeren`);
-          }
-        )
-      );
-  });
+            httpHeaderAccept: 'blob' as any,
+            context: undefined,
+          })
+          .pipe(
+            tapResponse(
+              (response) => {
+                const contentDisposition = response.headers.get('Content-Disposition');
+                const fileName = contentDisposition
+                  ? this.getFileNameFromContentDisposition(contentDisposition)
+                  : 'report.csv';
+                saveAs(response.body, fileName);
+              },
+              (e) => {
+                console.error(e);
+                this.notificationService.showError($localize`Kunne ikke hente rapporten`);
+              },
+              () => this.setLoading(false)
+            )
+          );
+      })
+    )
+  );
+
+  private getFileNameFromContentDisposition(contentDisposition: string): string {
+    const matches = /filename\*=utf-8''([^"]+)/.exec(contentDisposition);
+    return matches ? matches[1] : 'report.csv';
+  }
 }
--- src/app/modules/global-admin/global-admin-other/global-admin-other-broken-links/global-admin-other-broken-links.component.ts
+++ src/app/modules/global-admin/global-admin-other/global-admin-other-broken-links/global-admin-other-broken-links.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GlobalAdminOtherBrokenLinksComponentStore } from './global-admin-other-broken-links.component-store';
 
 @Component({
   selector: 'app-global-admin-other-broken-links',
   templateUrl: './global-admin-other-broken-links.component.html',
-  styleUrl: './global-admin-other-broken-links.component.scss'
+  styleUrl: './global-admin-other-broken-links.component.scss',
+  providers: [GlobalAdminOtherBrokenLinksComponentStore],
 })
-export class GlobalAdminOtherBrokenLinksComponent {
+export class GlobalAdminOtherBrokenLinksComponent implements OnInit {
+  public readonly isLoading$ = this.componentStore.isLoading$;
+  public readonly status$ = this.componentStore.status$;
 
+  constructor(public componentStore: GlobalAdminOtherBrokenLinksComponentStore) {}
+
+  ngOnInit(): void {
+    this.componentStore.getStatus();
+  }
+
+  public downloadReport() {
+    this.componentStore.getReport();
+  }
 }
